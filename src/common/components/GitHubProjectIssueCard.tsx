2be4885bbb48c1fdfacd886e4a2d60126d5623cc
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:15 -0400

    improve

--- src/common/components/GitHubProjectIssueCard.tsx
+++ src/common/components/GitHubProjectIssueCard.tsx
@@ -7,46 +7,71 @@ import {
   SxProps,
   Typography,
 } from '@mui/joy';
-import { string } from 'prop-types';
+import { string, node, shape } from 'prop-types';
 
-export const GitHubProjectIssueCard = (props: {
-  issue: number;
+export interface GitHubProjectIssueCardProps {
+  issue: string;
   text: string;
   note?: string | React.ReactNode;
   note2?: string | React.ReactNode;
   sx?: SxProps;
-}) => (
-  <Card variant="outlined" color="primary" sx={props.sx}>
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-      <GitHubIcon />
-      <Typography level="body-sm">
-        <MuiLink
-          overlay
-          href={`https://github.com/enricoros/big-AGI/issues/${props.issue}`}
-          target="_blank"
-          fontSize="1.25rem"
-          sx={{ textDecoration: 'none' }}
-        >
-          <Typography color="neutral">big-AGI #{props.issue}</Typography>
-        </MuiLink>
-        {' · '}
-        <Typography color="neutral">{props.text}.</Typography>
-      </Typography>
-    </Box>
-    {!!props.note && (
-      <Typography level="body-sm" sx={{ mt: 1 }}>
-        {props.note}
-      </Typography>
-    )}
-    {!!props.note2 && (
-      <Typography level="body-sm" sx={{ mt: 1 }}>
-        {props.note2}
-      </Typography>
-    )}
-  </Card>
-);
+  title?: string;
+}
+
+export const GitHubProjectIssueCard = React.forwardRef<
+  HTMLDivElement,
+  GitHubProjectIssueCardProps
+>((props, ref) => {
+  const { issue, text, note, note2, sx, title, ...other } = props;
+
+  return (
+    <Card
+      variant="outlined"
+      color="primary"
+      ref={ref}
+      sx={sx}
+      {...other}
+      key={issue}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <GitHubIcon />
+        <Typography level="body-sm">
+          <MuiLink
+            overlay
+            href={`https://github.com/enricoros/big-AGI/issues/${issue}`}
+            target="_blank"
+            fontSize="1.25rem"
+            underline="none"
+            title={title}
+            component="a"
+            sx={{ textDecoration: 'none' }}
+          >
+            <Typography color="neutral">big-AGI #{issue}</Typography>
+          </MuiLink>
+          {' · '}
+          <Typography color="neutral">{text}.</Typography>
+        </Typography>
+      </Box>
+      {!!note && (
+        <Typography level="body-sm" sx={{ mt: 1 }}>
+          {note}
+        </Typography>
+      )}
+      {!!note2 && (
+        <Typography level="body-sm" sx={{ mt: 1 }}>
+          {note2}
+        </Typography>
+      )}
+    </Card>
+  );
+});
 
 GitHubProjectIssueCard.propTypes = {
   issue: string.isRequired,
   text: string.isRequired,
+  note: node,
+  note2: node,
+  sx: shape({}),
+  title: string,
 };
+
