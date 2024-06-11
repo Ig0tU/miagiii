6ea61f5c6f9ddfc75e34a2828a399bffb804a413
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:20 -0400

    improve

--- src/apps/personas/creator/FromText.tsx
+++ src/apps/personas/creator/FromText.tsx
@@ -1,68 +1,82 @@
 import * as React from 'react';
-
-import { Box, Button, Textarea, Typography } from '@mui/joy';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  Stack,
+  Textarea,
+  Typography,
+} from '@mui/joy';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
-
 import { lineHeightTextareaMd } from '~/common/app.theme';
-
 import type { SimplePersonaProvenance } from '../store-app-personas';
 
-
-// minimum number of characters required to create from text
 const MIN_CHARS = 100;
 
-
-export function FromText(props: {
+interface FromTextProps {
   isCreating: boolean;
   onCreate: (text: string, provenance: SimplePersonaProvenance) => void;
-}) {
+}
 
-  // state
+export const FromText: React.FC<FromTextProps> = (props) => {
   const [text, setText] = React.useState('');
+  const [helperText, setHelperText] = React.useState('');
 
   const handleCreateFromText = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // stop the form submit
-    props.onCreate(text, { type: 'text' });
+    e.preventDefault();
+    if (text.length >= MIN_CHARS) {
+      props.onCreate(text, { type: 'text' });
+    } else {
+      setHelperText(`Minimum characters required: ${MIN_CHARS}`);
+    }
   };
 
-  return <>
-
-    <Typography level='title-md' startDecorator={<TextFieldsIcon />} sx={{ mb: 3 }}>
-      <b>Text</b> -&gt; Persona
-    </Typography>
-
-    <form onSubmit={handleCreateFromText}>
-      <Textarea
-        required
-        variant='outlined'
-        minRows={4} maxRows={8}
-        placeholder='Paste your text (e.g. tweets, social media, etc.) here...'
-        value={text}
-        onChange={event => setText(event.target.value)}
-        sx={{
-          backgroundColor: 'background.level1',
-          '&:focus-within': {
-            backgroundColor: 'background.popup',
-          },
-          lineHeight: lineHeightTextareaMd,
-          mb: 1.5,
-        }}
-      />
-
+  return (
+    <Stack spacing={3}>
+      <FormControl>
+        <InputLabel>
+          <Typography level='title-md' startDecorator={<TextFieldsIcon />}>
+            <b>Text</b> -&gt; Persona
+          </Typography>
+        </InputLabel>
+        <Textarea
+          required
+          variant='outlined'
+          minRows={4}
+          maxRows={8}
+          placeholder='Paste your text (e.g. tweets, social media, etc.) here...'
+          value={text}
+          onChange={(event) => {
+            setText(event.target.value);
+            setHelperText('');
+          }}
+          sx={{
+            backgroundColor: 'background.level1',
+            '&:focus-within': {
+              backgroundColor: 'background.popup',
+            },
+            lineHeight: lineHeightTextareaMd,
+            mb: 1.5,
+          }}
+        />
+        <FormHelperText error={helperText.length > 0}>{helperText}</FormHelperText>
+      </FormControl>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <Button
-          type='submit' variant='solid'
-          disabled={props.isCreating || text?.length < MIN_CHARS}
+          type='submit'
+          variant='solid'
+          disabled={props.isCreating || text.length < MIN_CHARS}
           sx={{ minWidth: 140 }}
+          onClick={handleCreateFromText}
         >
           Create
         </Button>
-
         <Typography level='body-sm'>
-          {text.length < MIN_CHARS ? `(${MIN_CHARS - text.length})` : text.length.toLocaleString()}
+          {text.length.toLocaleString()}
         </Typography>
       </Box>
-    </form>
-
-  </>;
-}
+    </Stack>
+  );
+};
