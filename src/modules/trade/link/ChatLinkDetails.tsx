ae14df262fa82aa71f8cb2e9602b32f5a296aa4f
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:11 -0400

    improve

--- src/modules/trade/link/ChatLinkDetails.tsx
+++ src/modules/trade/link/ChatLinkDetails.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import TimeAgo from 'react-timeago';
-
-import { Box, Button, Card, IconButton, Input, Stack, Tooltip, Typography } from '@mui/joy';
+import {
+  Box,
+  Button,
+  Card,
+  IconButton,
+  Input,
+  Stack,
+  Tooltip,
+  Typography,
+} from '@mui/joy';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import DoneIcon from '@mui/icons-material/Done';
@@ -9,30 +17,28 @@ import EditRoundedIcon from '@mui/icons-material/EditRounded';
 import IosShareIcon from '@mui/icons-material/IosShare';
 import LaunchIcon from '@mui/icons-material/Launch';
 import LinkIcon from '@mui/icons-material/Link';
-
-import { Brand } from '~/common/app.config';
+import Brand from '~/common/app.config';
 import { ConfirmationModal } from '~/common/components/ConfirmationModal';
 import { GoodModal } from '~/common/components/GoodModal';
 import { InlineError } from '~/common/components/InlineError';
 import { InlineTextarea } from '~/common/components/InlineTextarea';
-import { Link } from '~/common/components/Link';
+import { Link as RouterLink } from '~/common/components/Link';
 import { apiAsyncNode } from '~/common/util/trpc.client';
 import { copyToClipboard } from '~/common/util/clipboardUtils';
 import { getChatLinkRelativePath } from '~/common/app.routes';
 import { getOriginUrl } from '~/common/util/urlUtils';
 import { webShare, webSharePresent } from '~/common/util/pwaUtils';
-
 import type { StorageDeleteSchema, StoragePutSchema } from '../server/link';
 import { forgetChatLinkItem } from './store-link';
 
+interface ChatLinkDetailsProps {
+  open: boolean;
+  onClose: () => void;
+  storageItem: StoragePutSchema;
+  onChangeDeletionKey: (deletionKey: string) => void;
+}
 
-export function ChatLinkDetails(props: {
-  open: boolean,
-  onClose: () => void,
-  storageItem: StoragePutSchema,
-  onChangeDeletionKey: (deletionKey: string) => void,
-}) {
-
+export function ChatLinkDetails(props: ChatLinkDetailsProps) {
   // state
   const [opened, setOpened] = React.useState(false);
   const [copied, setCopied] = React.useState(false);
@@ -44,7 +50,12 @@ export function ChatLinkDetails(props: {
   // in case of 'put' error, just display the message
   if (props.storageItem.type === 'error') {
     return (
-      <GoodModal title='❌ Upload Error' dividers open={props.open} onClose={props.onClose}>
+      <GoodModal
+        title='❌ Upload Error'
+        dividers
+        open={props.open}
+        onClose={props.onClose}
+      >
         <InlineError error={props.storageItem.error} />
       </GoodModal>
     );
@@ -55,13 +66,9 @@ export function ChatLinkDetails(props: {
   const relativeUrl = getChatLinkRelativePath(objectId);
   const fullUrl = getOriginUrl() + relativeUrl;
 
-
   // Deletion Key Edit
-
   const handleKeyEditBegin = () => setIsEditingDeletionKey(true);
-
   const handleKeyEditCancel = () => setIsEditingDeletionKey(false);
-
   const handleKeyEditChange = (text: string) => {
     if (text) {
       setIsEditingDeletionKey(false);
@@ -70,177 +77,188 @@ export function ChatLinkDetails(props: {
   };
 
   // Deletion Key Copy
-
   const handleKeyCopy = () => {
     copyToClipboard(deletionKey, 'Link Deletion Key');
   };
 
-
-  const onOpen = () => setOpened(true);
-
-  const onCopy = () => {
+  const handleOpen = () => setOpened(true);
+  const handleCopy = () => {
     copyToClipboard(fullUrl, 'Public link');
     setCopied(true);
   };
 
-  const onNativeShare = () => webShare(
-    Brand.Title.Base + (dataTitle ? ` - ${dataTitle}` : ' - Shared Chat'),
-    'Check this out!',
-    fullUrl,
-    () => setNative(true),
-  );
+  const handleNativeShare = () =>
+    webShare(
+      `${Brand.Title.Base}${dataTitle ? ` - ${dataTitle}` : ''} - Shared Chat`,
+      'Check this out!',
+      fullUrl,
+      () => setNative(true)
+    );
 
-  const onDeleteNow = () => setConfirmDeletion(true);
+  const handleDeleteNow = () => setConfirmDeletion(true);
+  const handleDeleteCancelled = () => setConfirmDeletion(false);
 
-  const onDeleteCancelled = () => setConfirmDeletion(false);
-
-  const onConfirmedDeletion = async () => {
-    const result: StorageDeleteSchema = await apiAsyncNode.trade.storageDelete.mutate({ objectId, deletionKey });
+  const handleConfirmedDeletion = async () => {
+    const result: StorageDeleteSchema = await apiAsyncNode.trade.storageDelete.mutate({
+      objectId,
+      deletionKey,
+    });
     setDeletionResponse(result);
-    if (result.type === 'success')
-      forgetChatLinkItem(objectId);
+    if (result.type === 'success') forgetChatLinkItem(objectId);
     setConfirmDeletion(false);
   };
 
   const tryDeleted = !!deletionResponse;
   const isDeleted = deletionResponse?.type === 'success';
 
-
   return (
-    <GoodModal title='🔗 Link created' strongerTitle noTitleBar={isDeleted} dividers={!isDeleted} open onClose={props.onClose}>
+    <GoodModal
+      title={isDeleted ? '🗑️ Link deleted' : '🔗 Link created'}
+      strongerTitle={isDeleted}
+      dividers={!isDeleted}
+      open
+      onClose={props.onClose}
+    >
+      {!tryDeleted && (
+        <Card variant='solid' color='primary' invertedColors>
+          <Typography level='title-md'>🚀 Ready to share</Typography>
+          <Typography level='body-sm'>{fullUrl}</Typography>
 
-      {/* Success */}
-      {!tryDeleted && <Card variant='solid' color='primary' invertedColors>
-
-        <Typography level='title-md'>
-          🚀 Ready to share
-        </Typography>
-        <Typography level='body-sm'>
-          {fullUrl}
-        </Typography>
-
-        <Stack direction='row' gap={1}>
-          <Tooltip title='Open the link in a new tab'>
-            <Button
-              variant={opened ? 'soft' : 'solid'} onClick={onOpen}
-              color={opened ? 'success' : undefined} endDecorator={opened ? <DoneIcon /> : <LaunchIcon />}
-              component={Link} href={relativeUrl} noLinkStyle
-              sx={{ flexGrow: 1 }}
-            >
-              Open
-            </Button>
-          </Tooltip>
-
-          <Tooltip title='Copy the link to your clipboard'>
-            <Button
-              variant={copied ? 'soft' : 'solid'} onClick={onCopy}
-              color={copied ? 'success' : undefined} endDecorator={copied ? <DoneIcon /> : <LinkIcon />}
-              sx={{ flexGrow: 1 }}
-            >
-              Copy
-            </Button>
-          </Tooltip>
-
-          {webSharePresent() &&
-            <Tooltip title='Share the link using your device'>
+          <Stack direction='row' gap={1}>
+            <Tooltip title='Open the link in a new tab'>
               <Button
-                variant={native ? 'soft' : 'solid'} onClick={onNativeShare}
-                color={native ? 'success' : undefined} endDecorator={native ? <DoneIcon /> : <IosShareIcon />}
+                variant={opened ? 'soft' : 'solid'}
+                onClick={handleOpen}
+                color={opened ? 'success' : undefined}
+                endDecorator={opened ? <DoneIcon /> : <LaunchIcon />}
+                component={RouterLink}
+                href={relativeUrl}
+                noLinkStyle
                 sx={{ flexGrow: 1 }}
               >
-                Share
+                Open
               </Button>
-            </Tooltip>}
-        </Stack>
+            </Tooltip>
 
-      </Card>}
+            <Tooltip title='Copy the link to your clipboard'>
+              <Button
+                variant={copied ? 'soft' : 'solid'}
+                onClick={handleCopy}
+                color={copied ? 'success' : undefined}
+                endDecorator={copied ? <DoneIcon /> : <LinkIcon />}
+                sx={{ flexGrow: 1 }}
+              >
+                Copy
+              </Button>
+            </Tooltip>
 
-      {/* Deleted */}
-      {isDeleted && <Card variant='solid' color='danger' invertedColors>
-        <Typography level='title-md'>
-          🗑️ Link deleted
-        </Typography>
-        <Typography level='body-sm'>
-          This link has been deleted
-        </Typography>
-      </Card>}
+            {webSharePresent() && (
+              <Tooltip title='Share the link using your device'>
+                <Button
+                  variant={native ? 'soft' : 'solid'}
+                  onClick={handleNativeShare}
+                  color={native ? 'success' : undefined}
+                  endDecorator={native ? <DoneIcon /> : <IosShareIcon />}
+                  sx={{ flexGrow: 1 }}
+                >
+                  Share
+                </Button>
+              </Tooltip>
+            )}
+          </Stack>
+        </Card>
+      )}
 
-      {/* Deletion and Expiration */}
-      {!isDeleted && <Card variant='soft'>
+      {isDeleted && (
+        <Card variant='solid' color='danger' invertedColors>
+          <Typography level='title-md'>🗑️ Link deleted</Typography>
+          <Typography level='body-sm'>This link has been deleted</Typography>
+        </Card>
+      )}
 
-        <Typography level='title-sm'>
-          Deletion Key
-        </Typography>
+      {!isDeleted && (
+        <Card variant='soft'>
+          <Typography level='title-sm'>Deletion Key</Typography>
 
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          {isEditingDeletionKey ? (
-            <InlineTextarea
-              invertedColors
-              initialText={deletionKey}
-              onEdit={handleKeyEditChange}
-              onCancel={handleKeyEditCancel}
-              sx={{
-                flexGrow: 1,
-                ml: -1.5, mr: -0.5,
-              }}
-            />
-          ) : (
-            <Input
-              readOnly
-              variant='plain'
-              value={deletionKey}
-              endDecorator={
-                <Box sx={{ display: 'flex', gap: 2 }}>
-                  <Tooltip title='Edit Deletion Key'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            {isEditingDeletionKey ? (
+              <InlineTextarea
+                invertedColors
+                initialText={deletionKey}
+                onEdit={handleKeyEditChange}
+                onCancel={handleKeyEditCancel}
+                sx={{
+                  flexGrow: 1,
+                  ml: -1.5,
+                  mr: -0.5,
+                }}
+              />
+            ) : (
+              <Input
+                readOnly
+                variant='plain'
+                value={deletionKey}
+                endDecorator={
+                  <Box sx={{ display: 'flex', gap: 2 }}>
+                    <Tooltip title='Edit Deletion Key'>
+                      <IconButton
+                        variant='soft'
+                        color='primary'
+                        disabled={isEditingDeletionKey}
+                        onClick={handleKeyEditBegin}
+                      >
+                        <EditRoundedIcon />
+                      </IconButton>
+                    </Tooltip>
                     <IconButton
                       variant='soft'
                       color='primary'
                       disabled={isEditingDeletionKey}
-                      onClick={handleKeyEditBegin}
+                      onClick={handleKeyCopy}
                     >
-                      <EditRoundedIcon />
+                      <ContentCopyIcon />
                     </IconButton>
-                  </Tooltip>
-                  <IconButton
-                    variant='soft'
-                    color='primary'
-                    disabled={isEditingDeletionKey}
-                    onClick={handleKeyCopy}
-                  >
-                    <ContentCopyIcon />
-                  </IconButton>
-                </Box>
-              }
-              sx={{ flexGrow: 1 }}
-            />
-          )}
-        </Box>
+                  </Box>
+                }
+                sx={{ flexGrow: 1 }}
+              />
+            )}
+          </Box>
 
+          <Typography level='body-sm'>
+            IMPORTANT - <b>keep this key safe</b>, you will need it if you decide to delete the link at a later time,
+            and it will not appear again once you close this dialog.
+          </Typography>
 
-        <Typography level='body-sm'>
-          IMPORTANT - <b>keep this key safe</b>, you will need it if you decide to delete the link at a later time,
-          and it will not appear again once you close this dialog.
-        </Typography>
+          <Stack
+            direction='row'
+            gap={1}
+            sx={{ alignItems: 'center', justifyContent: 'space-between' }}
+          >
+            {expiresAt && (
+              <Typography level='title-sm'>
+                This chat will auto-expire <TimeAgo date={expiresAt} />.
+              </Typography>
+            )}
+            <Button
+              variant='outlined'
+              color='neutral'
+              endDecorator={<DeleteForeverIcon />}
+              onClick={handleDeleteNow}
+            >
+              Delete Now
+            </Button>
+          </Stack>
+        </Card>
+      )}
 
-        <Stack direction='row' gap={1} sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
-          {!!expiresAt && (
-            <Typography level='title-sm'>
-              This chat will auto-expire <TimeAgo date={expiresAt} />.
-            </Typography>
-          )}
-          <Button variant='outlined' color='neutral' endDecorator={<DeleteForeverIcon />} onClick={onDeleteNow}>
-            Delete Now
-          </Button>
-        </Stack>
-
-      </Card>}
-
-      {/* Delete confirmation */}
       <ConfirmationModal
-        open={confirmDeletion} onClose={onDeleteCancelled} onPositive={onConfirmedDeletion}
-        confirmationText={'Are you sure you want to delete this link?'} positiveActionText={'Yes, Delete'}
+        open={confirmDeletion}
+        onClose={handleDeleteCancelled}
+        onPositive={handleConfirmedDeletion}
+        confirmationText={'Are you sure you want to delete this link?'}
+        positiveActionText={'Yes, Delete'}
       />
-
     </GoodModal>
   );
 }
