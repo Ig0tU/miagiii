25a459ef19401857e094f36a9fa83af6018f7ce0
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:13 -0400

    improve

--- src/modules/llms/vendors/ollama/OllamaAdministration.tsx
+++ src/modules/llms/vendors/ollama/OllamaAdministration.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react';
 
-import { Box, Button, Chip, FormControl, IconButton, Input, Option, Select, Typography } from '@mui/joy';
+import {
+  Box,
+  Button,
+  Chip,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputAdornment,
+  Option,
+  Select,
+  Typography,
+} from '@mui/joy';
 import LaunchIcon from '@mui/icons-material/Launch';
 import FormatListNumberedRtlIcon from '@mui/icons-material/FormatListNumberedRtl';
 
@@ -13,21 +25,35 @@ import { apiQuery } from '~/common/util/trpc.client';
 
 import type { OllamaAccessSchema } from '../../server/ollama/ollama.router';
 
+type Props = {
+  access: OllamaAccessSchema;
+  onClose: () => void;
+};
 
-export function OllamaAdministration(props: { access: OllamaAccessSchema, onClose: () => void }) {
-
+export function OllamaAdministration(props: Props) {
   // state
   const [sortByPulls, setSortByPulls] = React.useState<boolean>(false);
   const [modelName, setModelName] = React.useState<string | null>('llama3');
   const [modelTag, setModelTag] = React.useState<string>('');
 
   // external state
-  const { data: pullableData } = apiQuery.llmOllama.adminListPullable.useQuery({ access: props.access }, {
-    staleTime: 1000 * 60,
-    refetchOnWindowFocus: false,
-  });
+  const { data: pullableData } = apiQuery.llmOllama.adminListPullable.useQuery(
+    { access: props.access },
+    {
+      staleTime: 1000 * 60,
+      refetchOnWindowFocus: false,
+    }
+  );
   const { data: pullData, isLoading: isPulling, status: pullStatus, error: pullError, mutate: pullMutate, reset: pullReset } = apiQuery.llmOllama.adminPull.useMutation();
   const { isLoading: isDeleting, status: deleteStatus, error: deleteError, mutate: deleteMutate, reset: deleteReset } = apiQuery.llmOllama.adminDelete.useMutation();
+
+  // reset mutations on unmount
+  React.useEffect(() => {
+    return () => {
+      pullReset();
+      deleteReset();
+    };
+  }, [pullReset, deleteReset]);
 
   // derived state
   let pullable = pullableData?.pullable || [];
@@ -35,41 +61,76 @@ export function OllamaAdministration(props: { access: OllamaAccessSchema, onClos
     pullable = pullable.toSorted((a, b) => b.pulls - a.pulls);
   const pullModelDescription = pullable.find(p => p.id === modelName)?.description ?? null;
 
-
-  const handleModelPull = () => {
+  const handleModelPull = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     deleteReset();
-    modelName && pullMutate({ access: props.access, name: modelName + (modelTag ? ':' + modelTag : '') });
+    if (modelName) pullMutate({ access: props.access, name: modelName + (modelTag ? ':' + modelTag : '') });
   };
 
-  const handleModelDelete = () => {
+  const handleModelDelete = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     pullReset();
-    modelName && deleteMutate({ access: props.access, name: modelName + (modelTag ? ':' + modelTag : '') });
+    if (modelName) deleteMutate({ access: props.access, name: modelName + (modelTag ? ':' + modelTag : '') });
   };
-
 
   return (
-    <GoodModal title='Ollama Administration' dividers open onClose={props.onClose}>
-
-      <Box sx={{ display: 'grid', gap: 'var(--Card-padding)' }}>
-        <Typography level='body-sm'>
+    <GoodModal
+      title='Ollama Administration'
+      dividers
+      open
+      onClose={props.onClose}
+    >
+      <Box
+        sx={{
+          display: 'grid',
+          gap: 'var(--Card-padding)',
+        }}
+      >
+        <Typography
+          level='body-sm'
+        >
           We assume your Ollama host is running and models are already available.
           However we provide a way to pull models from the Ollama host, for convenience.
         </Typography>
 
-        <Box sx={{ display: 'flex', flexFlow: 'row wrap', gap: 1 }}>
-          <FormControl sx={{ flexGrow: 1, flexBasis: 0.55 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexFlow: 'row wrap',
+            gap: 1,
+          }}
+        >
+          <FormControl
+            sx={{
+              flexGrow: 1,
+              flexBasis: 0.55,
+            }}
+          >
             <FormLabelStart title='Name' />
-            <Box sx={{ display: 'flex', gap: 1 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                gap: 1,
+              }}
+            >
               <Select
                 value={modelName || ''}
                 onChange={(_event: any, value: string | null) => setModelName(value)}
-                sx={{ flexGrow: 1 }}
+                sx={{
+                  flexGrow: 1,
+                }}
               >
-                {pullable.map(p =>
-                  <Option key={p.id} value={p.id}>
-                    {p.isNew === true && <Chip size='sm' variant='solid'>NEW</Chip>} {p.label}{sortByPulls && ` (${p.pulls.toLocaleString()})`}
-                  </Option>,
-                )}
+                {pullable.map((p) => (
+                  <Option
+                    key={p.id}
+                    value={p.id}
+                    select
+                  >
+                    {p.isNew === true && <Chip size='sm' variant='solid'>NEW</Chip>}
+                    {p.label}
+                    {sortByPulls && ` (${p.pulls.toLocaleString()})`}
+                  </Option>
+                ))}
               </Select>
               <GoodTooltip title='Sort by Downloads'>
                 <IconButton
@@ -81,71 +142,122 @@ export function OllamaAdministration(props: { access: OllamaAccessSchema, onClos
               </GoodTooltip>
             </Box>
           </FormControl>
-          <FormControl sx={{ flexGrow: 1, flexBasis: 0.45 }}>
+          <FormControl
+            sx={{
+              flexGrow: 1,
+              flexBasis: 0.45,
+            }}
+          >
             <FormLabelStart title='Tag' />
-            <Box sx={{ display: 'flex', gap: 1 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                gap: 1,
+              }}
+            >
               <Input
-                variant='outlined' placeholder='latest'
-                value={modelTag || ''} onChange={event => setModelTag(event.target.value)}
-                sx={{ minWidth: 80, flexGrow: 1 }}
-                slotProps={{ input: { size: 10 } }} // halve the min width
+                variant='outlined'
+                placeholder='latest'
+                value={modelTag || ''}
+                onChange={(event) => setModelTag(event.target.value)}
+                sx={{
+                  minWidth: 80,
+                  flexGrow: 1,
+                }}
+                slotProps={{
+                  input: {
+                    size: 10,
+                  },
+                }}
+                endDecorator={
+                  !!modelName && (
+                    <IconButton
+                      component={Link}
+                      href={`https://ollama.ai/library/${modelName}`}
+                      target='_blank'
+                    >
+                      <LaunchIcon />
+                    </IconButton>
+                  )
+                }
               />
-              {!!modelName && (
-                <IconButton
-                  component={Link} href={`https://ollama.ai/library/${modelName}`} target='_blank'
-                >
-                  <LaunchIcon />
-                </IconButton>
-              )}
             </Box>
           </FormControl>
         </Box>
 
-
         {/* Status*/}
-        {!!pullData && (pullData.error
-          ? <Typography color='danger'>{pullData.error}</Typography>
-          : <Typography color='success'>{pullData.status || 'Ok, but unkown status'}</Typography>)}
+        {!!pullData && pullData.error && (
+          <FormHelperText
+            error
+            marginTop={0}
+            marginBottom={0}
+          >
+            {pullData.error}
+          </FormHelperText>
+        )}
         {!!pullError && <InlineError error={pullError} />}
         {!!deleteError && <InlineError error={deleteError} />}
 
-
         {/* Description and Buttons */}
-        <Box sx={{ display: 'flex', gap: 1, justifyContent: 'space-between' }}>
-
-          <Typography level='body-sm'>
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 1,
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography
+            level='body-sm'
+          >
             {pullModelDescription}
           </Typography>
 
-          <Box sx={{ display: 'flex', flexWrap: 1, gap: 1, alignItems: 'start' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 1,
+              gap: 1,
+              alignItems: 'start',
+            }}
+          >
             <Button
               variant='outlined'
               color={deleteStatus === 'error' ? 'danger' : deleteStatus === 'success' ? 'success' : 'primary'}
-              loading={isDeleting} disabled={isPulling} onClick={handleModelDelete}
-              sx={{ minWidth: 100 }}
+              loading={isDeleting}
+              disabled={isPulling}
+              onClick={handleModelDelete}
+              sx={{
+                minWidth: 100,
+              }}
             >
               Delete
             </Button>
 
             <Button
               color={pullStatus === 'error' ? 'danger' : pullStatus === 'success' ? 'success' : 'primary'}
-              loading={isPulling} disabled={isDeleting} onClick={handleModelPull}
-              sx={{ minWidth: 100 }}
+              loading={isPulling}
+              disabled={isDeleting}
+              onClick={handleModelPull}
+              sx={{
+                minWidth: 100,
+              }}
             >
               Pull
             </Button>
           </Box>
-
         </Box>
 
         {/* Warnings */}
-        {isPulling && <Typography color='warning' level='body-sm'>
-          Pulling maybe slow and TIME OUT as the operation will download many GBs from the internet. In case of a
-          timeout, the server is still downloading the model. Check back again later and the model should be available.
-        </Typography>}
-
+        {isPulling && (
+          <Typography
+            color='warning'
+            level='body-sm'
+          >
+            Pulling maybe slow and TIME OUT as the operation will download many GBs from the internet. In case of a
+            timeout, the server is still downloading the model. Check back again later and the model should be available.
+          </Typography>
+        )}
       </Box>
-
     </GoodModal>
   );
 }
