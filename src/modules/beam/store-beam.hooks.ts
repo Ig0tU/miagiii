e6a9cc775baeaff2a01313d0dd1c96246bc5bbe1
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:55 -0400

    improve

--- src/modules/beam/store-beam.hooks.ts
+++ src/modules/beam/store-beam.hooks.ts
@@ -3,58 +3,46 @@ import { type StoreApi, useStore } from 'zustand';
 
 import type { BeamStore } from './store-beam-vanilla';
 
-
 export type BeamStoreApi = Readonly<StoreApi<BeamStore>>;
-
 
 export const useBeamStore = <T, >(beamStore: BeamStoreApi, selector: (store: BeamStore) => T): T =>
   useStore(beamStore, selector);
 
-/*export const useIsBeamOpen = (beamStore?: BeamStoreApi) => {
-  const [open, setOpen] = React.useState(false);
+export const useAreBeamsOpen = (beamStores: (BeamStoreApi | null)[]): boolean[] => {
+  const [opens, setOpens] = React.useState<boolean[]>(() => Array.from({ length: beamStores.length }, () => false));
+  const previousBeamStoresRef = React.useRef(beamStores);
 
-  // attach to the current beamStore
+  const setOpensMemoized = React.useCallback((index: number, isOpen: boolean) => {
+    setOpens((opens) => {
+      const nextOpens = [...opens];
+      nextOpens[index] = isOpen;
+      return nextOpens;
+    });
+  }, []);
+
   React.useEffect(() => {
-    if (!beamStore) {
-      setOpen(false);
+    if (previousBeamStoresRef.current === beamStores) {
       return;
     }
-    setOpen(beamStore.getState().isOpen);
-    return beamStore.subscribe((state: BeamState, prevState: BeamState) => {
-      (state.isOpen !== prevState.isOpen) && setOpen(state.isOpen);
-    });
-  }, [beamStore]);
 
-  return open;
-};*/
+    previousBeamStoresRef.current = beamStores;
 
-export const useAreBeamsOpen = (beamStores: (BeamStoreApi | null)[]): boolean[] => {
-  const [opens, setOpens] = React.useState<boolean[]>([]);
-
-  React.useEffect(() => {
-    // Reflect the current state
-    setOpens(beamStores.map((beamStore) => !!beamStore?.getState().isOpen));
-
-    // Attach to the current beamStores
     const unsubscribes = beamStores.map((beamStore, index) => {
       if (!beamStore) {
         return () => {
         }; // Explicitly return a no-op function for clarity
       }
-      return beamStore.subscribe((state: BeamStore, prevState: BeamStore) => {
-        if (state.isOpen !== prevState.isOpen) {
-          setOpens((opens) => {
-            const nextOpens = [...opens];
-            nextOpens[index] = state.isOpen;
-            return nextOpens;
-          });
+      let previousState = beamStore.getState();
+      return beamStore.subscribe((state: BeamStore) => {
+        if (state.isOpen !== previousState.isOpen) {
+          setOpensMemoized(index, state.isOpen);
         }
+        previousState = state;
       });
     });
 
-    // Cleanup: unsubscribe from all subscriptions when the component unmounts or beamStores changes
     return () => unsubscribes.forEach((unsubscribe) => unsubscribe());
-  }, [beamStores]);
+  }, [beamStores, setOpensMemoized]);
 
   return opens;
 };
