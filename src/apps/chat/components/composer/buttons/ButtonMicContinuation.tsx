065e079f4184368c33b756b23ee9b12c39aed073
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:57 -0400

    improve

--- src/apps/chat/components/composer/buttons/ButtonMicContinuation.tsx
+++ src/apps/chat/components/composer/buttons/ButtonMicContinuation.tsx
@@ -1,22 +1,58 @@
 import * as React from 'react';
 
-import { Box, IconButton, Tooltip } from '@mui/joy';
-import { ColorPaletteProp, SxProps, VariantProp } from '@mui/joy/styles/types';
+import { Box, IconButton, Tooltip, Typography } from '@mui/joy';
+import { ColorPaletteProp, SxProps, VariantProp, SizeProp, DisableProp, RefProp } from '@mui/joy/styles/types';
 import AutoModeIcon from '@mui/icons-material/AutoMode';
 
+type MicContinuationLegendProps = {
+  sx?: SxProps;
+};
 
-const micContinuationLegend =
-  <Box sx={{ px: 1, py: 0.75, lineHeight: '1.5rem' }}>
-    Voice Continuation
-  </Box>;
+const MicContinuationLegend = (props: MicContinuationLegendProps) => {
+  return (
+    <Box sx={{ px: 1, py: 0.75, lineHeight: '1.5rem', ...props.sx }}>
+      <Typography level="body2">Voice Continuation</Typography>
+    </Box>
+  );
+};
 
+type ButtonMicContinuationProps = {
+  variant: VariantProp;
+  color: ColorPaletteProp;
+  onClick: () => void;
+  disabled?: DisableProp;
+  disabledSx?: SxProps;
+  disabledTitle?: string;
+  sx?: SxProps;
+  size?: SizeProp;
+};
 
 export const ButtonMicContinuationMemo = React.memo(ButtonMicContinuation);
 
-function ButtonMicContinuation(props: { variant: VariantProp, color: ColorPaletteProp, onClick: () => void, sx?: SxProps }) {
-  return <Tooltip placement='bottom' title={micContinuationLegend}>
-    <IconButton variant={props.variant} color={props.color} onClick={props.onClick} sx={props.sx}>
-      <AutoModeIcon />
-    </IconButton>
-  </Tooltip>;
+function ButtonMicContinuation(props: ButtonMicContinuationProps) {
+  return (
+    <Tooltip
+      placement="bottom"
+      title={
+        props.disabled ? (
+          <MicContinuationLegend sx={{ color: 'neutral.400' }} />
+        ) : (
+          <MicContinuationLegend />
+        )
+      }
+    >
+      <IconButton
+        variant={props.variant}
+        color={props.color}
+        onClick={props.disabled ? undefined : props.onClick}
+        disabled={props.disabled}
+        sx={props.disabled ? { ...props.sx, ...props.disabledSx, color: 'neutral.400' } : props.sx}
+        size={props.size}
+        ref={props.disabled ? undefined : props.ref}
+        title={props.disabled ? props.disabledTitle : undefined}
+      >
+        <AutoModeIcon />
+      </IconButton>
+    </Tooltip>
+  );
 }
