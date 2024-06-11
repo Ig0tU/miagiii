0c6f7352d05c86a05f53646f28728f8814c1caa0
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:27 -0400

    improve

--- middleware_BASIC_AUTH.ts
+++ middleware_BASIC_AUTH.ts
@@ -8,35 +8,38 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
+const { HTTP_BASIC_AUTH_USERNAME, HTTP_BASIC_AUTH_PASSWORD } = process.env;
 
 // noinspection JSUnusedGlobalSymbols
 export function middleware(request: NextRequest) {
+  const authHeader = request.headers.get('authorization') as string | null;
 
   // Validate deployment configuration
-  if (!process.env.HTTP_BASIC_AUTH_USERNAME || !process.env.HTTP_BASIC_AUTH_PASSWORD) {
+  if (!HTTP_BASIC_AUTH_USERNAME || !HTTP_BASIC_AUTH_PASSWORD) {
     console.warn('HTTP Basic Authentication is enabled but not configured');
     return new Response('Unauthorized/Unconfigured', unauthResponse);
   }
 
   // Request client authentication if no credentials are provided
-  const authHeader = request.headers.get('authorization');
   if (!authHeader?.startsWith('Basic '))
     return new Response('Unauthorized', unauthResponse);
 
   // Request authentication if credentials are invalid
   const base64Credentials = authHeader.split(' ')[1];
   const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-  const [username, password] = credentials.split(':');
+  const [username, password] = (() => {
+    const obj = Object.fromEntries(credentials.split(':'));
+    return [obj.username || '', obj.password || ''] as [string, string];
+  })();
+
   if (
-    !username || !password ||
-    username !== process.env.HTTP_BASIC_AUTH_USERNAME ||
-    password !== process.env.HTTP_BASIC_AUTH_PASSWORD
+    username !== HTTP_BASIC_AUTH_USERNAME ||
+    password !== HTTP_BASIC_AUTH_PASSWORD
   )
     return new Response('Unauthorized', unauthResponse);
 
   return NextResponse.next();
 }
-
 
 // Response to send when authentication is required
 const unauthResponse: ResponseInit = {
