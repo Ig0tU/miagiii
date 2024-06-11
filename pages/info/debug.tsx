d45ea8e563cf371d821b0ed7eabe420d84414a4f
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:14:32 -0400

    improve

--- pages/info/debug.tsx
+++ pages/info/debug.tsx
@@ -1,54 +1,67 @@
 import * as React from 'react';
 import { fileSave } from 'browser-fs-access';
-
 import { Box, Button, Card, CardContent, Typography } from '@mui/joy';
 import DownloadIcon from '@mui/icons-material/Download';
+import {
+  Brand,
+  ROUTE_APP_CHAT,
+  ROUTE_INDEX,
+} from '~/common/app.config';
+import {
+  AppPlaceholder,
+  getBackendCapabilities,
+  getLLMsDebugInfo,
+  getPlantUmlServerUrl,
+  withLayout,
+} from '~/common';
+import {
+  useAppNewsStateStore,
+  useAppStateStore,
+  useCapabilityBrowserSpeechRecognition,
+  useCapabilityElevenLabs,
+  useCapabilityTextToImage,
+  useChatStore,
+  useFolderStore,
+  useUXLabsStore,
+} from '~/common/state/store';
+import {
+  clientHostName,
+  isChromeDesktop,
+  isFirefox,
+  isIPhoneUser,
+  isMacUser,
+  isPwa,
+  isVercelFromFrontend,
+} from '~/common/util/pwaUtils';
+import {
+  getGA4MeasurementId,
+  prettyTimestampForFilenames,
+  supportsClipboardRead,
+  supportsScreenCapture,
+} from '~/common/util';
 
-import { AppPlaceholder } from '../../src/apps/AppPlaceholder';
+interface DebugCardProps {
+  title: string;
+  children: React.ReactNode;
+}
 
-import { getBackendCapabilities } from '~/modules/backend/store-backend-capabilities';
-import { getPlantUmlServerUrl } from '~/modules/blocks/code/RenderCode';
-
-import { withLayout } from '~/common/layout/withLayout';
-
-
-// app config
-import { Brand } from '~/common/app.config';
-import { ROUTE_APP_CHAT, ROUTE_INDEX } from '~/common/app.routes';
-
-// apps access
-import { incrementalNewsVersion, useAppNewsStateStore } from '../../src/apps/news/news.version';
-
-// capabilities access
-import { useCapabilityBrowserSpeechRecognition, useCapabilityElevenLabs, useCapabilityTextToImage } from '~/common/components/useCapabilities';
-
-// stores access
-import { getLLMsDebugInfo } from '~/modules/llms/store-llms';
-import { useAppStateStore } from '~/common/state/store-appstate';
-import { useChatStore } from '~/common/state/store-chats';
-import { useFolderStore } from '~/common/state/store-folders';
-import { useUXLabsStore } from '~/common/state/store-ux-labs';
-
-// utils access
-import { clientHostName, isChromeDesktop, isFirefox, isIPhoneUser, isMacUser, isPwa, isVercelFromFrontend } from '~/common/util/pwaUtils';
-import { getGA4MeasurementId } from '~/common/components/GoogleAnalytics';
-import { prettyTimestampForFilenames } from '~/common/util/timeUtils';
-import { supportsClipboardRead } from '~/common/util/clipboardUtils';
-import { supportsScreenCapture } from '~/common/util/screenCaptureUtils';
-
-
-function DebugCard(props: { title: string, children: React.ReactNode }) {
+function DebugCard({ title, children }: DebugCardProps) {
   return (
     <Box>
-      <Typography level='title-lg'>
-        {props.title}
+      <Typography level="title-lg">
+        {title}
       </Typography>
-      {props.children}
+      {children}
     </Box>
   );
 }
 
-function prettifyJsonString(jsonString: string, deleteChars: number, removeDoubleQuotes: boolean, removeTrailComma: boolean): string {
+function prettifyJsonString(
+  jsonString: string,
+  deleteChars: number,
+  removeDoubleQuotes: boolean,
+  removeTrailComma: boolean
+): string {
   return jsonString.split('\n').map(l => {
     if (deleteChars > 0)
       l = l.substring(deleteChars);
@@ -60,41 +73,45 @@ function prettifyJsonString(jsonString: string, deleteChars: number, removeDoubl
   }).join('\n').trim();
 }
 
-function DebugJsonCard(props: { title: string, data: any }) {
+interface DebugJsonCardProps {
+  title: string;
+  data: any;
+}
+
+function DebugJsonCard({ title, data }: DebugJsonCardProps) {
   return (
-    <DebugCard title={props.title}>
-      <Typography level='body-sm' sx={{ whiteSpace: 'break-spaces', fontFamily: 'code', fontSize: { xs: 'xs' } }}>
-        {prettifyJsonString(JSON.stringify(props.data, null, 2), 2, true, true)}
+    <DebugCard title={title}>
+      <Typography level="body-sm" sx={{ whiteSpace: 'break-spaces', fontFamily: 'code', fontSize: { xs: 'xs' } }}>
+        {prettifyJsonString(JSON.stringify(data, null, 2), 2, true, true)}
       </Typography>
     </DebugCard>
   );
 }
 
+interface AppDebugProps {}
 
-function AppDebug() {
-
+function AppDebug(props: AppDebugProps) {
   // state
   const [saved, setSaved] = React.useState(false);
 
   // external state
   const backendCaps = getBackendCapabilities();
   const chatsCount = useChatStore.getState().conversations?.length;
-  const uxLabsExperiments = Object.entries(useUXLabsStore.getState()).filter(([_k, v]) => v === true).map(([k, _]) => k).join(', ');
+  const uxLabsExperiments = Object.entries(useUXLabsStore.getState()).filter(([_, v]) => v === true).map(([k, _]) => k).join(', ');
   const { folders, enableFolders } = useFolderStore.getState();
   const { lastSeenNewsVersion } = useAppNewsStateStore.getState();
   const { usageCount } = useAppStateStore.getState();
 
-
   // derived state
   const cClient = {
-    // isBrowser,
-    isChromeDesktop,
-    isFirefox,
-    isIPhone: isIPhoneUser,
-    isMac: isMacUser,
+    // isBrowser: isBrowser(),
+    isChromeDesktop: isChromeDesktop(),
+    isFirefox: isFirefox(),
+    isIPhone: isIPhoneUser(),
+    isMac: isMacUser(),
     isPWA: isPwa(),
     supportsClipboardPaste: supportsClipboardRead,
-    supportsScreenCapture,
+    supportsScreenCapture: supportsScreenCapture(),
   };
   const cProduct = {
     capabilities: {
@@ -127,12 +144,16 @@ function AppDebug() {
   };
 
   const handleDownload = async () => {
-    fileSave(
-      new Blob([JSON.stringify({ client: cClient, agi: cProduct, backend: cBackend }, null, 2)], { type: 'application/json' }),
-      { fileName: `big-agi_debug_${prettyTimestampForFilenames()}.json`, extensions: ['.json'] },
-    )
-      .then(() => setSaved(true))
-      .catch(e => console.error('Error saving debug.json', e));
+    try {
+      const blob = new Blob([JSON.stringify({ client: cClient, agi: cProduct, backend: cBackend }, null, 2)], { type: 'application/json' });
+      await fileSave(blob, {
+        fileName: `big-agi_debug_${prettyTimestampForFilenames()}.json`,
+        extensions: ['.json'],
+      });
+      setSaved(true);
+    } catch (e) {
+      console.error('Error saving debug.json', e);
+    }
   };
 
   return (
@@ -163,7 +184,6 @@ function AppDebug() {
   );
 }
 
-
 export default function DebugPage() {
   return withLayout({ type: 'plain' }, <AppDebug />);
-};
+}
