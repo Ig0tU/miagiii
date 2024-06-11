9e84aac4afc6cdaba761324bae03f527d9accb02
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:06 -0400

    improve

--- src/common/layout/optima/components/MobileNavIcon.tsx
+++ src/common/layout/optima/components/MobileNavIcon.tsx
@@ -1,51 +1,60 @@
-import { Box, IconButton, styled } from '@mui/joy';
+import { Box, IconButton, styled, type CSSObject } from '@mui/joy';
 
-
-export const MobileNavGroupBox = styled(Box)({
-  // layout
-  flex: 1,
-  minHeight: 'var(--Bar)',
-
-  // contents
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'space-evenly',
-  alignItems: 'center',
-
-  // style
-  // backgroundColor: 'rgba(0 0 0 / 0.5)', // darken bg
-
-  // debug
-  // '& > *': { border: '1px solid red' },
-});
-
-export const mobileNavItemClasses = {
+const mobileNavItemClasses = {
   typeApp: 'NavButton-typeApp',
   active: 'NavButton-active',
 };
 
-export const MobileNavIcon = styled(IconButton)(({ theme }) => ({
+const mobileNavGroupBoxClasses: CSSObject = {
+  root: {
+    // layout
+    flex: 1,
+    minHeight: 'var(--Bar)',
 
-  // custom vars
-  '--MarginY': '0.5rem',
-  '--ExtraPadX': '1rem',
+    // contents
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
 
-  // IconButton customization
-  '--Icon-fontSize': '1.25rem',
-  '--IconButton-size': 'calc(var(--Bar) - 2 * var(--MarginY))',
-  paddingInline: 'var(--ExtraPadX)',
-  border: 'none',
+    // style
+    // backgroundColor: 'rgba(0 0 0 / 0.5)', // darken bg
 
-  [`&.${mobileNavItemClasses.typeApp}:hover`]: {
-    backgroundColor: 'var(--variant-solidHoverBg)',
-    // backgroundColor: theme.palette.neutral.softHoverBg,
-    color: theme.palette.neutral.softColor,
+    // debug
+    // '& > *': { border: '1px solid red' },
   },
+};
 
-  // app active (non hover)
-  // [`&.${mobileNavItemClasses.typeApp}.${mobileNavItemClasses.active}`]: {
-  //   backgroundColor: ...
-  // },
+const mobileNavIconClasses: CSSObject = {
+  root: {
+    // custom vars
+    '--MarginY': '0.5rem',
+    '--ExtraPadX': '1rem',
 
-})) as typeof IconButton;
+    // IconButton customization
+    '--Icon-fontSize': '1.25rem',
+    '--IconButton-size': 'calc(var(--Bar) - 2 * var(--MarginY))',
+    paddingInline: 'var(--ExtraPadX)',
+    border: 'none',
+
+    '&:hover': {
+      backgroundColor: 'var(--variant-solidHoverBg)',
+      color: 'var(--neutral-softColor)',
+    },
+  };
+};
+
+export const MobileNavGroupBox = styled(Box)(mobileNavGroupBoxClasses);
+
+export const MobileNavIcon = styled(IconButton)(
+  mobileNavIconClasses,
+  ({ theme }) => ({
+    '&.${mobileNavItemClasses.typeApp}': {
+      '--IconButton-color': theme.palette.neutral.softColor,
+      '&:hover': {
+        backgroundColor: theme.palette.neutral.softHoverBg,
+      },
+    },
+  })
+) as typeof IconButton;
