d8eda41f2d80d51d51873044d9c7427e015b4023
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:59 -0400

    improve

--- src/server/api/trpc.server.ts
+++ src/server/api/trpc.server.ts
@@ -6,35 +6,26 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
+
 import { initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
 
-/**
- * 1. CONTEXT
- *
- * This section defines the "contexts" that are available in the backend API.
- *
- * These allow you to access things when processing a request, like the database, the session, etc.
- */
-export const createTRPCFetchContext = ({ req /*, resHeaders*/ }: { req: Request; resHeaders: Headers; }) => {
-  // const user = { name: req.headers.get('username') ?? 'anonymous' };
-  // return { req, resHeaders };
+// 1. CONTEXT
+
+type FetchContext = {
+  hostName: string;
+};
+
+export const createTRPCFetchContext = ({ req }: { req: Request; }) => {
   return {
-    // only used by Backend Analytics
     hostName: req.headers?.get('host') ?? 'localhost',
   };
 };
 
+// 2. INITIALIZATION
 
-/**
- * 2. INITIALIZATION
- *
- * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
- * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
- * errors on the backend.
- */
-const t = initTRPC.context<typeof createTRPCFetchContext>().create({
+const t = initTRPC<FetchContext>().context<typeof createTRPCFetchContext>().create({
   transformer: superjson,
   errorFormatter({ shape, error }) {
     return {
@@ -48,25 +39,7 @@ const t = initTRPC.context<typeof createTRPCFetchContext>().create({
   },
 });
 
-/**
- * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
- *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
- * "/src/server/api/routers" directory.
- */
+// 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
 
-/**
- * This is how you create new routers and sub-routers in your tRPC API.
- *
- * @see https://trpc.io/docs/router
- */
 export const createTRPCRouter = t.router;
-
-/**
- * Public (unauthenticated) procedure
- *
- * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
- * guarantee that a user querying is authorized, but you can still access user session data if they
- * are logged in.
- */
 export const publicProcedure = t.procedure;
