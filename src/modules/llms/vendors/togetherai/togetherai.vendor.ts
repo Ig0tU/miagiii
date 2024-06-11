d227b4e843a0eaaa341b559088ef6778265e2a1a
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:14:23 -0400

    improve

--- src/modules/llms/vendors/togetherai/togetherai.vendor.ts
+++ src/modules/llms/vendors/togetherai/togetherai.vendor.ts
@@ -1,13 +1,8 @@
 import { TogetherIcon } from '~/common/components/icons/vendors/TogetherIcon';
 
-import type { IModelVendor } from '../IModelVendor';
-import type { OpenAIAccessSchema } from '../../server/openai/openai.router';
-
 import { LLMOptionsOpenAI, ModelVendorOpenAI } from '../openai/openai.vendor';
 import { OpenAILLMOptions } from '../openai/OpenAILLMOptions';
-
 import { TogetherAISourceSetup } from './TogetherAISourceSetup';
-
 
 export interface SourceSetupTogetherAI {
   togetherKey: string;
@@ -15,7 +10,7 @@ export interface SourceSetupTogetherAI {
   togetherFreeTrial: boolean;
 }
 
-export const ModelVendorTogetherAI: IModelVendor<SourceSetupTogetherAI, OpenAIAccessSchema, LLMOptionsOpenAI> = {
+export const ModelVendorTogetherAI: IModelVendor<SourceSetupTogetherAI, any, LLMOptionsOpenAI> = {
   id: 'togetherai',
   name: 'Together AI',
   rank: 17,
@@ -29,47 +24,53 @@ export const ModelVendorTogetherAI: IModelVendor<SourceSetupTogetherAI, OpenAIAc
   LLMOptionsComponent: OpenAILLMOptions,
 
   // functions
-  initializeSetup: () => ({
-    togetherKey: '',
-    togetherHost: 'https://api.together.xyz',
-    togetherFreeTrial: false,
-  }),
-  validateSetup: (setup) => {
+  initializeSetup(): SourceSetupTogetherAI {
+    return {
+      togetherKey: '',
+      togetherHost: 'https://api.together.xyz',
+      togetherFreeTrial: false,
+    };
+  },
+  validateSetup(setup: SourceSetupTogetherAI) {
     return setup.togetherKey?.length >= 64;
   },
-  getTransportAccess: (partialSetup) => ({
-    dialect: 'togetherai',
-    oaiKey: partialSetup?.togetherKey || '',
-    oaiOrg: '',
-    oaiHost: partialSetup?.togetherHost || '',
-    heliKey: '',
-    moderationCheck: false,
-  }),
+  getTransportAccess(partialSetup: Partial<SourceSetupTogetherAI>) {
+    if (!partialSetup) {
+      throw new Error('Missing partialSetup');
+    }
 
-  // there is delay for OpenRouter Free API calls
-  getRateLimitDelay: (_llm, partialSetup) => {
+    return {
+      dialect: 'togetherai',
+      oaiKey: partialSetup?.togetherKey || '',
+      oaiOrg: '',
+      oaiHost: partialSetup?.togetherHost || '',
+      heliKey: '',
+      moderationCheck: false,
+    };
+  },
+
+  getRateLimitDelay(_llm: any, partialSetup: Partial<SourceSetupTogetherAI>) {
     const now = Date.now();
-    const elapsed = now - nextGenerationTs;
+    const elapsed = now - (partialSetup.nextGenerationTs || 0);
     const wait = partialSetup?.togetherFreeTrial
       ? 1000 + 50 /* 1 seconds for free call, plus some safety margin */
       : 50;
 
     if (elapsed < wait) {
       const delay = wait - elapsed;
-      nextGenerationTs = now + delay;
+      partialSetup.nextGenerationTs = now + delay;
       return delay;
     } else {
-      nextGenerationTs = now;
+      partialSetup.nextGenerationTs = now;
       return 0;
     }
   },
-
 
   // OpenAI transport ('togetherai' dialect in 'access')
   rpcUpdateModelsOrThrow: ModelVendorOpenAI.rpcUpdateModelsOrThrow,
   rpcChatGenerateOrThrow: ModelVendorOpenAI.rpcChatGenerateOrThrow,
   streamingChatGenerateOrThrow: ModelVendorOpenAI.streamingChatGenerateOrThrow,
-};
 
-// rate limit timestamp
-let nextGenerationTs = 0;
+  // rate limit timestamp
+  nextGenerationTs: 0,
+};
