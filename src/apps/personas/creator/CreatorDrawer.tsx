a342c4c97966f25d35587047f80e58564a3134a6
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:00 -0400

    improve

--- src/apps/personas/creator/CreatorDrawer.tsx
+++ src/apps/personas/creator/CreatorDrawer.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
-
-import { Box, Button, IconButton, ListItemDecorator, Sheet, Tooltip } from '@mui/joy';
+import {
+  Box,
+  Button,
+  IconButton,
+  ListItemDecorator,
+  Sheet,
+  Tooltip,
+} from '@mui/joy';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
@@ -10,16 +16,20 @@ import DoneIcon from '@mui/icons-material/Done';
 import { PageDrawerHeader } from '~/common/layout/optima/components/PageDrawerHeader';
 import { PageDrawerList } from '~/common/layout/optima/components/PageDrawerList';
 import { useOptimaDrawers } from '~/common/layout/optima/useOptimaDrawers';
-
 import { CreatorDrawerItem } from './CreatorDrawerItem';
-import { deleteSimplePersona, deleteSimplePersonas, useSimplePersonas } from '../store-app-personas';
+import {
+  deleteSimplePersona,
+  deleteSimplePersonas,
+  useSimplePersonas,
+} from '../store-app-personas';
 
-
-export function CreatorDrawer(props: {
-  selectedSimplePersonaId: string | null,
-  setSelectedSimplePersonaId: (simplePersonaId: string | null) => void,
+export function CreatorDrawer({
+  selectedSimplePersonaId,
+  setSelectedSimplePersonaId,
+}: {
+  selectedSimplePersonaId: string | null;
+  setSelectedSimplePersonaId: (simplePersonaId: string | null) => void;
 }) {
-
   // selection mode
   const [selectMode, setSelectMode] = React.useState(false);
   const [selectedIds, setSelectedIds] = React.useState<Set<string>>(new Set());
@@ -28,14 +38,10 @@ export function CreatorDrawer(props: {
   const { closeDrawer } = useOptimaDrawers();
   const { simplePersonas } = useSimplePersonas();
 
-
   // derived state
   const hasPersonas = simplePersonas.length > 0;
 
-
   // Simple Persona Operations
-
-  const { setSelectedSimplePersonaId } = props;
 
   const handleSimplePersonaUnselect = React.useCallback(() => {
     setSelectedSimplePersonaId(null);
@@ -46,7 +52,6 @@ export function CreatorDrawer(props: {
     handleSimplePersonaUnselect();
   }, [handleSimplePersonaUnselect]);
 
-
   // Selection
 
   const handleSelectionClose = React.useCallback(() => {
@@ -55,24 +60,26 @@ export function CreatorDrawer(props: {
   }, []);
 
   const handleSelectionToggleId = React.useCallback((simplePersonaId: string) => {
-    setSelectedIds(prevSelectedIds => {
+    setSelectedIds((prevSelectedIds) => {
       const newSelectedItems = new Set(prevSelectedIds);
-      if (newSelectedItems.has(simplePersonaId))
+      if (newSelectedItems.has(simplePersonaId)) {
         newSelectedItems.delete(simplePersonaId);
-      else
+      } else {
         newSelectedItems.add(simplePersonaId);
+      }
       return newSelectedItems;
     });
   }, []);
 
   const handleSelectionInvert = React.useCallback(() => {
-    setSelectedIds(prevSelectedIds => {
+    setSelectedIds((prevSelectedIds) => {
       const newSelectedIds = new Set(prevSelectedIds);
-      simplePersonas.forEach(persona => {
-        if (newSelectedIds.has(persona.id))
+      simplePersonas.forEach((persona) => {
+        if (newSelectedIds.has(persona.id)) {
           newSelectedIds.delete(persona.id);
-        else
+        } else {
           newSelectedIds.add(persona.id);
+        }
       });
       return newSelectedIds;
     });
@@ -83,105 +90,126 @@ export function CreatorDrawer(props: {
     setSelectedIds(new Set());
   }, [selectedIds]);
 
-
-  return <>
-
-    {/* Drawer Header */}
-    <PageDrawerHeader
-      title={selectMode ? 'Selection Mode' : 'Recent'}
-      onClose={selectMode ? handleSelectionClose : closeDrawer}
-    >
-      {hasPersonas && !selectMode && (
-        <Tooltip title={selectMode ? 'Done' : 'Select'}>
-          <IconButton onClick={selectMode ? handleSelectionClose : () => setSelectMode(true)}>
-            {selectMode ? <DoneIcon /> : <CheckBoxOutlineBlankIcon />}
-          </IconButton>
-        </Tooltip>
-      )}
-    </PageDrawerHeader>
-
-    <PageDrawerList
-      variant='plain'
-      noTopPadding noBottomPadding tallRows
-      onClick={handleSimplePersonaUnselect}
-    >
-
-      {selectMode ? (
-        // Selection Header
-        <Sheet variant='soft' color='warning' invertedColors>
-          <Box sx={{ display: 'flex', alignItems: 'center', px: 1, minHeight: '3rem' }}>
-            <Button
-              variant='plain'
-              color='warning'
-              startDecorator={selectedIds.size === simplePersonas.length ? <CheckBoxOutlineBlankIcon /> : <CheckBoxIcon />}
-              onClick={handleSelectionInvert}
+  return (
+    <>
+      {/* Drawer Header */}
+      <PageDrawerHeader
+        title={selectMode ? 'Selection Mode' : 'Recent'}
+        onClose={selectMode ? handleSelectionClose : closeDrawer}
+      >
+        {hasPersonas && !selectMode && (
+          <Tooltip title={selectMode ? 'Done' : 'Select'}>
+            <IconButton
+              onClick={selectMode ? handleSelectionClose : () => setSelectMode(true)}
             >
-              {selectedIds.size === simplePersonas.length
-                ? 'Select None'
-                : selectedIds.size === 0
+              {selectMode ? <DoneIcon /> : <CheckBoxOutlineBlankIcon />}
+            </IconButton>
+          </Tooltip>
+        )}
+      </PageDrawerHeader>
+
+      <PageDrawerList
+        variant="plain"
+        noTopPadding
+        noBottomPadding
+        tallRows
+        onClick={handleSimplePersonaUnselect}
+      >
+        {selectMode ? (
+          // Selection Header
+          <Sheet variant="soft" color="warning" invertedColors>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                px: 1,
+                minHeight: '3rem',
+              }}
+            >
+              <Button
+                variant="plain"
+                color="warning"
+                startDecorator={
+                  selectedIds.size === simplePersonas.length ? (
+                    <CheckBoxOutlineBlankIcon />
+                  ) : selectedIds.size === 0 ? (
+                    <CheckBoxIcon />
+                  ) : (
+                    <DoneIcon />
+                  )
+                }
+                onClick={handleSelectionInvert}
+              >
+                {selectedIds.size === simplePersonas.length
+                  ? 'Select None'
+                  : selectedIds.size === 0
                   ? `Select ${simplePersonas.length.toLocaleString() || 'All'}`
                   : 'Invert'}
-            </Button>
-            <Button
-              variant='solid'
-              color='warning'
-              startDecorator={<DeleteOutlineIcon />}
-              onClick={handleSelectionDelete}
-              disabled={selectedIds.size === 0}
-              sx={{ ml: 'auto' }}
-            >
-              Delete
-            </Button>
-          </Box>
-        </Sheet>
-      ) : (
-        // Create Button
-        <Button
-          variant={props.selectedSimplePersonaId ? 'plain' : 'soft'}
-          onClick={handleSimplePersonaUnselect}
-          sx={{
-            m: 2,
+              </Button>
+              <Button
+                variant="solid"
+                color="warning"
+                startDecorator={<DeleteOutlineIcon />}
+                onClick={handleSelectionDelete}
+                disabled={selectedIds.size === 0}
+                sx={{ ml: 'auto' }}
+              >
+                Delete
+              </Button>
+            </Box>
+          </Sheet>
+        ) : (
+          // Create Button
+          <Button
+            variant={
+              selectedSimplePersonaId ? 'plain' : 'soft'
+            }
+            onClick={handleSimplePersonaUnselect}
+            sx={{
+              m: 2,
 
-            // ...PageDrawerTallItemSx,
-            justifyContent: 'flex-start',
-            padding: '0px 0.75rem',
+              // ...PageDrawerTallItemSx,
+              justifyContent: 'flex-start',
+              padding: '0px 0.75rem',
 
-            // style
-            border: '1px solid',
-            borderColor: 'neutral.outlinedBorder',
-            borderRadius: 'sm',
-            '--ListItemDecorator-size': 'calc(2.5rem - 1px)', // compensate for the border
-          }}
-        >
-          <ListItemDecorator><Diversity2Icon /></ListItemDecorator>
-          {/*<Typography level='title-sm' sx={!props.selectedSimplePersonaId ? { fontWeight: 'lg' } : undefined}>*/}
-          Create
-          {/*</Typography>*/}
-        </Button>
-      )}
-
-      {/* Personas [] */}
-      <Box sx={{ flex: 1, overflowY: 'auto' }}>
-        {simplePersonas.map(item =>
-          <CreatorDrawerItem
-            key={item.id}
-            item={item}
-            isActive={item.id === props.selectedSimplePersonaId}
-            isSelected={selectedIds.has(item.id)}
-            isSelection={selectMode}
-            onClick={(event) => {
-              event.stopPropagation();
-              if (selectMode)
-                handleSelectionToggleId(item.id);
-              else
-                props.setSelectedSimplePersonaId(item.id);
+              // style
+              border: '1px solid',
+              borderColor: 'neutral.outlinedBorder',
+              borderRadius: 'sm',
+              '--ListItemDecorator-size': 'calc(2.5rem - 1px)', // compensate for the border
             }}
-            onDelete={handleSimplePersonaDelete}
-          />,
+          >
+            <ListItemDecorator>
+              <Diversity2Icon />
+            </ListItemDecorator>
+            {/*<Typography level='title-sm' sx={!props.selectedSimplePersonaId ? { fontWeight: 'lg' } : undefined}>*/}
+            Create
+            {/*</Typography>*/}
+          </Button>
         )}
-      </Box>
 
-    </PageDrawerList>
-
-  </>;
+        {/* Personas [] */}
+        <Box sx={{ flex: 1, overflowY: 'auto' }}>
+          {simplePersonas.map((item) => (
+            <CreatorDrawerItem
+              key={item.id}
+              item={item}
+              isActive={item.id === selectedSimplePersonaId}
+              isSelected={selectedIds.has(item.id)}
+              isSelection={selectMode}
+              onClick={(event) => {
+                event.stopPropagation();
+                if (selectMode) {
+                  handleSelectionToggleId(item.id);
+                } else {
+                  setSelectedSimplePersonaId(item.id);
+                }
+              }}
+              onDelete={handleSimplePersonaDelete}
+            />
+          ))}
+        </Box>
+      </PageDrawerList>
+    </>
+  );
 }
