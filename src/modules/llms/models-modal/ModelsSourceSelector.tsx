5b3aa56df97194fb86a1c934b58382ed36c9155f
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:56 -0400

    improve

--- src/modules/llms/models-modal/ModelsSourceSelector.tsx
+++ src/modules/llms/models-modal/ModelsSourceSelector.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import { shallow } from 'zustand/shallow';
 
-import { Badge, Box, Button, IconButton, ListItemDecorator, MenuItem, Option, Select, Typography } from '@mui/joy';
+import {
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  ListItemDecorator,
+  MenuItem,
+  Option,
+  Select,
+  Typography,
+} from '@mui/joy';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import CloudDoneOutlinedIcon from '@mui/icons-material/CloudDoneOutlined';
+import ComputerIcon from '@mui/icons-material/Computer';
+import CloudOutlinedIcon from '@mui/icons-material/CloudOutlined';
 
 import { getBackendCapabilities } from '~/modules/backend/store-backend-capabilities';
 
@@ -12,222 +25,221 @@ import { ConfirmationModal } from '~/common/components/ConfirmationModal';
 import { themeZIndexOverMobileDrawer } from '~/common/app.theme';
 import { useIsMobile } from '~/common/components/useMatchMedia';
 
-import type { IModelVendor } from '../vendors/IModelVendor';
-import { DModelSourceId, useModelsStore } from '../store-llms';
-import { createModelSourceForVendor, findAllVendors, findVendorById, ModelVendorId } from '../vendors/vendors.registry';
-import { vendorHasBackendCap } from '../vendors/useSourceSetup';
+import { IModelVendor, DModelSourceId, ModelVendorId } from '../vendors/IModelVendor';
+import { useModelsStore } from '../store-llms';
+import { createModelSourceForVendor, findAllVendors, findVendorById, vendorHasBackendCap } from '../vendors/vendors.registry';
 
-
-/*function locationIcon(vendor?: IModelVendor | null) {
-  if (vendor && vendor.id === 'openai' && vendorHasBackendCap(...))
-    return <CloudDoneOutlinedIcon />;
-  return !vendor ? null : vendor.location === 'local' ? <ComputerIcon /> : <CloudOutlinedIcon />;
-}*/
-
-function vendorIcon(vendor: IModelVendor | null, greenMark: boolean) {
-  let icon: React.JSX.Element | null = null;
-  if (vendor?.Icon)
+function VendorIcon({ vendor, greenMark }: { vendor: IModelVendor | null, greenMark: boolean }) {
+  let icon: React.ReactNode = null;
+  if (vendor?.Icon) {
     icon = <vendor.Icon />;
-  return (greenMark && icon)
-    ? <Badge size='sm' badgeContent='' slotProps={{ badge: { sx: { backgroundColor: 'lime', boxShadow: 'none', border: '1px solid gray', p: 0 } } }}>{icon}</Badge>
-    : icon;
+  }
+  return (
+    <Badge
+      size='sm'
+      badgeContent=''
+      slotProps={{
+        badge: {
+          sx: {
+            backgroundColor: 'lime',
+            boxShadow: 'none',
+            border: '1px solid gray',
+            p: 0,
+          },
+        },
+      }}
+    >
+      {greenMark && icon}
+    </Badge>
+  );
 }
 
-
-export function ModelsSourceSelector(props: {
-  selectedSourceId: DModelSourceId | null, setSelectedSourceId: (sourceId: DModelSourceId | null) => void,
+function ModelsSourceSelector({
+  selectedSourceId,
+  setSelectedSourceId,
+}: {
+  selectedSourceId: DModelSourceId | null;
+  setSelectedSourceId: (sourceId: DModelSourceId | null) => void;
 }) {
-
-  // state
   const [vendorsMenuAnchor, setVendorsMenuAnchor] = React.useState<HTMLElement | null>(null);
   const [confirmDeletionSourceId, setConfirmDeletionSourceId] = React.useState<DModelSourceId | null>(null);
 
-  // external state
   const isMobile = useIsMobile();
-  const { modelSources, addModelSource, removeModelSource } = useModelsStore(state => ({
-    modelSources: state.sources,
-    addModelSource: state.addSource, removeModelSource: state.removeSource,
-  }), shallow);
+  const { modelSources, addModelSource, removeModelSource } = useModelsStore(
+    (state) => ({
+      modelSources: state.sources,
+      addModelSource: state.addSource,
+      removeModelSource: state.removeSource,
+    }),
+    shallow
+  );
 
   const handleShowVendors = (event: React.MouseEvent<HTMLElement>) => setVendorsMenuAnchor(event.currentTarget);
-
   const closeVendorsMenu = () => setVendorsMenuAnchor(null);
 
-  const handleAddSourceFromVendor = React.useCallback((vendorId: ModelVendorId) => {
+  const handleAddSourceFromVendor = (vendorId: ModelVendorId) => {
     closeVendorsMenu();
     const { sources: modelSources } = useModelsStore.getState();
     const modelSource = createModelSourceForVendor(vendorId, modelSources);
     if (modelSource) {
       addModelSource(modelSource);
-      props.setSelectedSourceId(modelSource.id);
+      setSelectedSourceId(modelSource.id);
     }
-  }, [addModelSource, props]);
+  };
 
-
-  const enableDeleteButton = !!props.selectedSourceId && modelSources.length > 1;
+  const enableDeleteButton = !!selectedSourceId && modelSources.length > 1;
 
   const handleDeleteSource = (id: DModelSourceId) => setConfirmDeletionSourceId(id);
 
-  const handleDeleteSourceConfirmed = React.useCallback(() => {
+  const handleDeleteSourceConfirmed = () => {
     if (confirmDeletionSourceId) {
-      props.setSelectedSourceId(modelSources.find(source => source.id !== confirmDeletionSourceId)?.id ?? null);
+      setSelectedSourceId(modelSources.find((source) => source.id !== confirmDeletionSourceId)?.id ?? null);
       removeModelSource(confirmDeletionSourceId);
       setConfirmDeletionSourceId(null);
     }
-  }, [confirmDeletionSourceId, modelSources, props, removeModelSource]);
+  };
 
-
-  // vendor list items
   const vendorComponents = React.useMemo(() => {
-
-    // prepare the items
-    const vendorItems = findAllVendors()
-      .filter(v => !!v.instanceLimit)
+    const backendCaps = getBackendCapabilities();
+    return findAllVendors()
+      .filter((v) => !!v.instanceLimit)
       .sort((a, b) => {
-        // sort first by 'cloud' on top (vs. 'local'), then by name
-        // if (a.location !== b.location)
-        //   return a.location === 'cloud' ? -1 : 1;
+        if (a.location !== b.location) {
+          return a.location === 'cloud' ? -1 : 1;
+        }
         return a.name.localeCompare(b.name);
       })
-      .map(vendor => {
-          const sourceInstanceCount = modelSources.filter(source => source.vId === vendor.id).length;
-          const enabled = vendor.instanceLimit > sourceInstanceCount;
-          const backendCaps = getBackendCapabilities();
-          return {
-            vendor,
-            enabled,
-            component: (
-              <MenuItem key={vendor.id} disabled={!enabled} onClick={() => handleAddSourceFromVendor(vendor.id)}>
-                <ListItemDecorator>
-                  {vendorIcon(vendor, vendorHasBackendCap(vendor, backendCaps))}
-                </ListItemDecorator>
-                {vendor.name}
-
-                {/*{sourceInstanceCount > 0 && ` (added)`}*/}
-
-                {/* Free indication */}
-                {/*{!!vendor.hasFreeModels && ` 🎁`}*/}
-
-                {/* Multiple instance hint */}
-                {vendor.instanceLimit > 1 && !!sourceInstanceCount && enabled && (
-                  <Typography component='span' level='body-sm'>
-                    #{sourceInstanceCount + 1}
-                    {/*/{vendor.instanceLimit}*/}
-                  </Typography>
-                )}
-
-                {/* Local chip */}
-                {/*{vendor.location === 'local' && (*/}
-                {/*  <Chip variant='solid' size='sm'>*/}
-                {/*    local*/}
-                {/*  </Chip>*/}
-                {/*)}*/}
-              </MenuItem>
-            ),
-          };
-        },
-      );
-
-    // prepend headers
-    // const components: React.ReactNode[] = [];
-    // let lastLocation: 'cloud' | 'local' | null = null;
-    // vendorItems.forEach(item => {
-    //   if (item.vendor.location !== lastLocation) {
-    //     lastLocation = item.vendor.location;
-    //     components.push(
-    //       <Typography key={lastLocation} level='body-xs' sx={{
-    //         color: 'text.tertiary',
-    //         mx: 1.5,
-    //         mt: 1,
-    //         mb: 1,
-    //       }}>
-    //         {lastLocation === 'cloud' ? 'Cloud Services' : 'Local Services'}
-    //       </Typography>,
-    //     );
-    //   }
-    //   components.push(item.component);
-    // });
-    // return components;
-
-    return vendorItems.map(item => item.component);
-  }, [handleAddSourceFromVendor, modelSources]);
-
-  // source items
-  const sourceItems = React.useMemo(() => modelSources
-      .map(source => {
-        const icon = vendorIcon(findVendorById(source.vId), false);
+      .map((vendor) => {
+        const sourceInstanceCount = modelSources.filter((source) => source.vId === vendor.id).length;
+        const enabled = vendor.instanceLimit > sourceInstanceCount;
         return {
-          source,
-          icon,
+          vendor,
+          enabled,
           component: (
-            <Option key={source.id} value={source.id}>
-              {/*<ListItemDecorator>{icon}</ListItemDecorator>*/}
-              {source.label}
-            </Option>
+            <MenuItem
+              key={vendor.id}
+              disabled={!enabled}
+              onClick={() => handleAddSourceFromVendor(vendor.id)}
+            >
+              <ListItemDecorator>
+                <VendorIcon vendor={vendor} greenMark={vendorHasBackendCap(vendor, backendCaps)} />
+              </ListItemDecorator>
+              {vendor.name}
+              {sourceInstanceCount > 0 && ' (added)'}
+              {!!vendor.hasFreeModels && ' 🎁'}
+              {vendor.instanceLimit > 1 && !!sourceInstanceCount && enabled && (
+                <Typography component='span' level='body-sm'>
+                  #{sourceInstanceCount + 1}
+                </Typography>
+              )}
+              {vendor.location === 'local' && (
+                <Typography component='span' level='body-sm'>
+                  local
+                </Typography>
+              )}
+            </MenuItem>
           ),
         };
-      })
-      .sort((a, b) => a.source.label.localeCompare(b.source.label))
-    , [modelSources]);
+      });
+  }, [handleAddSourceFromVendor, modelSources]);
 
-  const selectedSourceItem = sourceItems.find(item => item.source.id === props.selectedSourceId);
+  const sourceItems = React.useMemo(() => {
+    return modelSources.map((source) => {
+      const icon = <VendorIcon vendor={findVendorById(source.vId)} greenMark={false} />;
+      return {
+        source,
+        icon,
+        component: (
+          <Option key={source.id} value={source.id}>
+            <ListItemDecorator>{icon}</ListItemDecorator>
+            {source.label}
+          </Option>
+        ),
+      };
+    });
+  }, [modelSources]);
+
+  const selectedSourceItem = sourceItems.find((item) => item.source.id === selectedSourceId);
   const noSources = !sourceItems.length;
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', gap: 1 }}>
-
-      {/* Models: [Select] Add Delete */}
-      {!isMobile && <Typography sx={{ mr: 1 }}>
-        Service:
-      </Typography>}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        gap: 1,
+      }}
+    >
+      {!isMobile && <Typography sx={{ mr: 1 }}>Service:</Typography>}
 
       <Select
         variant='outlined'
-        value={props.selectedSourceId}
+        value={selectedSourceId}
         disabled={noSources}
-        onChange={(_event, value) => value && props.setSelectedSourceId(value)}
+        onChange={(_event, value) => value && setSelectedSourceId(value)}
         startDecorator={selectedSourceItem?.icon}
         slotProps={{
           root: { sx: { minWidth: 190 } },
           indicator: { sx: { opacity: 0.5 } },
         }}
       >
-        {sourceItems.map(item => item.component)}
+        {sourceItems.map((item) => item.component)}
       </Select>
 
       {isMobile ? (
-        <IconButton variant={noSources ? 'solid' : 'plain'} color='primary' onClick={handleShowVendors} disabled={!!vendorsMenuAnchor}>
+        <IconButton
+          variant={noSources ? 'solid' : 'plain'}
+          color='primary'
+          onClick={handleShowVendors}
+          disabled={!!vendorsMenuAnchor}
+        >
           <AddIcon />
         </IconButton>
       ) : (
-        <Button variant={noSources ? 'solid' : 'plain'} onClick={handleShowVendors} disabled={!!vendorsMenuAnchor} startDecorator={<AddIcon />}>
+        <Button
+          variant={noSources ? 'solid' : 'plain'}
+          onClick={handleShowVendors}
+          disabled={!!vendorsMenuAnchor}
+          startDecorator={<AddIcon />}
+        >
           Add
         </Button>
       )}
 
       <IconButton
-        variant='plain' color='neutral' disabled={!enableDeleteButton} sx={{ ml: 'auto' }}
-        onClick={() => props.selectedSourceId && handleDeleteSource(props.selectedSourceId)}
+        variant='plain'
+        color='neutral'
+        disabled={!enableDeleteButton}
+        sx={{ ml: 'auto' }}
+        onClick={() => selectedSourceId && handleDeleteSource(selectedSourceId)}
       >
         <DeleteOutlineIcon />
       </IconButton>
 
-
-      {/* vendors popup, for adding */}
       <CloseableMenu
-        placement='bottom-start' zIndex={themeZIndexOverMobileDrawer}
-        open={!!vendorsMenuAnchor} anchorEl={vendorsMenuAnchor} onClose={closeVendorsMenu}
+        placement='bottom-start'
+        zIndex={themeZIndexOverMobileDrawer}
+        open={!!vendorsMenuAnchor}
+        anchorEl={vendorsMenuAnchor}
+        onClose={closeVendorsMenu}
         sx={{ minWidth: 200 }}
       >
         {vendorComponents}
       </CloseableMenu>
 
-      {/* source delete confirmation */}
       <ConfirmationModal
-        open={!!confirmDeletionSourceId} onClose={() => setConfirmDeletionSourceId(null)} onPositive={handleDeleteSourceConfirmed}
-        confirmationText={'Are you sure you want to remove these models? The configuration data will be lost and you may have to enter it again.'} positiveActionText={'Remove'}
+        open={!!confirmDeletionSourceId}
+        onClose={() => setConfirmDeletionSourceId(null)}
+        onPositive={handleDeleteSourceConfirmed}
+        confirmationText={
+          'Are you sure you want to remove these models? The configuration data will be lost and you may have to enter it again.'
+        }
+        positiveActionText={'Remove'}
       />
-
     </Box>
   );
 }
+
+export default ModelsSourceSelector;
