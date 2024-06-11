dc6107fb42bbc4dfb306a8879498371973c6cfa0
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:24 -0400

    improve

--- src/common/layout/optima/OptimaLayout.tsx
+++ src/common/layout/optima/OptimaLayout.tsx
@@ -1,74 +1,102 @@
 import * as React from 'react';
 import { useRouter } from 'next/router';
-import { PanelGroup } from 'react-resizable-panels';
+import { PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 import { checkVisibleNav, navItems } from '~/common/app.nav';
 import { useIsMobile } from '~/common/components/useMatchMedia';
 
-import { DesktopDrawer } from './DesktopDrawer';
-import { DesktopNav } from './DesktopNav';
-import { MobileDrawer } from './MobileDrawer';
-import { Modals } from './Modals';
+import { DesktopDrawer, DesktopDrawerProps } from './DesktopDrawer';
+import { DesktopNav, DesktopNavProps } from './DesktopNav';
+import { MobileDrawer, MobileDrawerProps } from './MobileDrawer';
+import { Modals, ModalsProps } from './Modals';
 import { OptimaDrawerProvider } from './useOptimaDrawers';
 import { OptimaLayoutProvider } from './useOptimaLayout';
-import { PageWrapper } from './PageWrapper';
+import { PageWrapper, PageWrapperProps } from './PageWrapper';
 
+export type OptimaLayoutProps = {
+  suspendAutoModelsSetup?: boolean;
+  children: React.ReactNode;
+};
 
-/**
- * Core layout of big-AGI, used by all the Primary applications therein.
- *
- * Main functions:
- *  - modern responsive layout
- *  - core layout of the application, with the Nav, Panes, PageBar, etc.
- *    - the child(ren) of this layout are placed in the main content area
- *  - allows for pluggable components of children applications, via usePluggableOptimaLayout
- *  - overlays and displays various modals
- *  - flicker free
- */
-export function OptimaLayout(props: { suspendAutoModelsSetup?: boolean, children: React.ReactNode }) {
-
+export function OptimaLayout({
+  suspendAutoModelsSetup,
+  children,
+}: OptimaLayoutProps): React.ReactElement {
   // external state
   const { route } = useRouter();
   const isMobile = useIsMobile();
 
   // derived state
-  const currentApp = navItems.apps.find(item => item.route === route);
+  const currentApp = navItems.apps.find((item) => item.route === route);
+
+  const desktopDrawerProps: DesktopDrawerProps = {
+    component: 'aside',
+    currentApp,
+    collapsed: !checkVisibleNav(currentApp),
+  };
+
+  const desktopNavProps: DesktopNavProps = {
+    component: 'nav',
+    currentApp,
+  };
+
+  const mobileDrawerProps: MobileDrawerProps = {
+    component: 'aside',
+    currentApp,
+  };
+
+  const modalsProps: ModalsProps = {
+    suspendAutoModelsSetup,
+  };
+
+  const pageWrapperProps: PageWrapperProps = {
+    component: 'main',
+    currentApp,
+  };
+
+  const panelGroupProps = {
+    direction: 'horizontal' as const,
+    id: 'root-layout',
+    resizeHandles: ['s'],
+    className: 'optima-layout',
+    style: {
+      display: 'grid',
+      gridTemplateColumns: 'auto 1fr',
+      gridTemplateRows: 'auto 1fr',
+      gridTemplateAreas: isMobile
+        ? `"nav nav" "drawer drawer" "main main"`
+        : `"nav nav" "drawer main" "drawer main"`,
+    },
+    defaultSize: isMobile ? 0 : 250,
+    minSize: 0,
+    maxSize: isMobile ? 100 : 500,
+    disabled: isMobile,
+    onChange: (size) => {
+      console.log('Panel size changed:', size);
+    },
+  };
 
   return (
     <OptimaLayoutProvider>
       <OptimaDrawerProvider>
-
-        {isMobile ? <>
-
-          <PageWrapper component='main' isMobile currentApp={currentApp}>
-            {props.children}
-          </PageWrapper>
-
-          <MobileDrawer component='aside' currentApp={currentApp} />
-
-        </> : (
-
-          <PanelGroup direction='horizontal' id='root-layout'>
-
-            {checkVisibleNav(currentApp) && <DesktopNav component='nav' currentApp={currentApp} />}
-
-            <DesktopDrawer component='aside' currentApp={currentApp} />
-
-            {/*<Panel defaultSize={100}>*/}
-            <PageWrapper component='main' currentApp={currentApp}>
-              {props.children}
+        {isMobile ? (
+          <>
+            <PageWrapper {...pageWrapperProps} key="main">
+              {children}
             </PageWrapper>
-            {/*</Panel>*/}
-
+            <MobileDrawer {...mobileDrawerProps} />
+          </>
+        ) : (
+          <PanelGroup {...panelGroupProps}>
+            {checkVisibleNav(currentApp) && <DesktopNav {...desktopNavProps} />}
+            <DesktopDrawer {...desktopDrawerProps} />
+            <PageWrapper {...pageWrapperProps} />
           </PanelGroup>
-
         )}
-
       </OptimaDrawerProvider>
-
-      {/* Overlay Modals */}
-      <Modals suspendAutoModelsSetup={props.suspendAutoModelsSetup} />
-
+      <Modals {...modalsProps} />
     </OptimaLayoutProvider>
   );
 }
+
+export { OptimaLayout as default };
