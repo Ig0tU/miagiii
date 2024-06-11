f828f9890685a6e6336431fc002703341b66607b
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:31 -0400

    improve

--- pages/workspace.tsx
+++ pages/workspace.tsx
@@ -1,17 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import { Box } from '@mui/joy';
 import { withLayout } from '~/common/layout/withLayout';
 
 // Replace this with the actual component for AppWorkspace
-// import { AppWorkspace } from '../src/apps/personas/AppWorkspace';
-const AppWorkspace = () => <div>AppWorkspace Placeholder</div>;
+const AppWorkspace: React.FC = () => <div>AppWorkspace Placeholder</div>;
 
-const PersonasPage = () => {
-  // You can pass the actual AppWorkspace component to withLayout
-  // instead of the Box component
-  const LayoutAppWorkspace = withLayout({ type: 'optima' }, AppWorkspace);
-
-  return <LayoutAppWorkspace />;
+export const PersonasPage: React.FC = () => {
+  return <withLayout({ type: 'optima' })(AppWorkspace) />;
 };
-
-export default PersonasPage;
