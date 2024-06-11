8d8b26ed85ee1dd50b4b38da62b33e00369fd3c2
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:35 -0400

    improve

--- src/common/components/GoodTooltip.tsx
+++ src/common/components/GoodTooltip.tsx
@@ -1,33 +1,42 @@
 import * as React from 'react';
 
-import { Tooltip } from '@mui/joy';
-import { SxProps } from '@mui/joy/styles/types';
+import { Tooltip, SxProps } from '@mui/joy';
 
+type GoodTooltipProps = {
+  title: string;
+  placement?: 'top' | 'bottom' | 'top-start';
+  isError?: boolean;
+  isWarning?: boolean;
+  arrow?: boolean;
+  usePlain?: boolean;
+  children: React.ReactNode;
+  sx?: SxProps;
+};
 
 /**
  * Tooltip with text that wraps to multiple lines (doesn't go too long)
  */
-export const GoodTooltip = (props: {
-  title: React.ReactNode,
-  placement?: 'top' | 'bottom' | 'top-start',
-  isError?: boolean, isWarning?: boolean,
-  arrow?: boolean,
-  usePlain?: boolean,
-  children: React.JSX.Element,
-  sx?: SxProps
-}) =>
-  <Tooltip
-    title={props.title}
-    placement={props.placement}
-    disableInteractive
-    arrow={props.arrow}
-    variant={(props.isError || props.isWarning) ? 'soft' : props.usePlain ? 'plain' : undefined}
-    color={props.isError ? 'danger' : props.isWarning ? 'warning' : undefined}
-    sx={{
-      maxWidth: { sm: '50vw', md: '25vw' },
-      whiteSpace: 'break-spaces',
-      ...props.sx,
-    }}
-  >
-    {props.children}
-  </Tooltip>;
+const GoodTooltip = (props: GoodTooltipProps) => {
+  const { title, placement = 'top', isError, isWarning, arrow = true, usePlain = false, children, sx } = props;
+
+  return (
+    <Tooltip
+      key={title}
+      title={title}
+      placement={placement}
+      disableInteractive
+      arrow={arrow}
+      variant={isError || isWarning ? 'soft' : usePlain ? 'plain' : undefined}
+      color={isError ? 'danger' : isWarning ? 'warning' : undefined}
+      sx={{
+        maxWidth: { sm: '50vw', md: '25vw' },
+        whiteSpace: 'break-spaces',
+        ...(sx || {}),
+      }}
+    >
+      {children}
+    </Tooltip>
+  );
+};
+
+export default GoodTooltip;
