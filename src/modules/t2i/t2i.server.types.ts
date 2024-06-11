03cb6a5ef518c7b5be4cc68ab60da4f95bc9c4be
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:14:51 -0400

    improve

--- src/modules/t2i/t2i.server.types.ts
+++ src/modules/t2i/t2i.server.types.ts
@@ -1,10 +1,20 @@
-import { z } from 'zod';
-
+import { ZodArray, ZodObject, z } from 'zod';
 
 // Image generation output
 
-const t2iCreateImageOutputSchema = z.object({
+interface IT2iCreateImageOutput {
+  // one of these two will be present
+  imageUrl?: string;
+  base64ImageDataUrl?: string;
 
+  // could be the revised prompt, or an alt textual description of the image
+  altText: string;
+
+  // optional
+  elapsed?: number;
+}
+
+const t2iCreateImageOutputSchema = ZodObject<IT2iCreateImageOutput>({
   // one of these two will be present
   imageUrl: z.string().optional(),
   base64ImageDataUrl: z.string().optional(),
@@ -14,7 +24,6 @@ const t2iCreateImageOutputSchema = z.object({
 
   // optional
   elapsed: z.number().optional(),
-
 });
 
-export const t2iCreateImagesOutputSchema = z.array(t2iCreateImageOutputSchema);
+export const t2iCreateImagesOutputSchema = ZodArray(t2iCreateImageOutputSchema);
