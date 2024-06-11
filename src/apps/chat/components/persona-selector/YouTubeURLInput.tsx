827ed768be34f280a77f3ef6db489f1807b1d067
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:33 -0400

    improve

--- src/apps/chat/components/persona-selector/YouTubeURLInput.tsx
+++ src/apps/chat/components/persona-selector/YouTubeURLInput.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-
-import { Box, Button, Input } from '@mui/joy';
+import { Box, Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/joy';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
 import type { SxProps } from '@mui/joy/styles/types';
 import { useYouTubeTranscript, YTVideoTranscript } from '~/modules/youtube/useYouTubeTranscript';
-
 
 interface YouTubeURLInputProps {
   onSubmit: (transcript: string) => void;
@@ -16,12 +14,13 @@ interface YouTubeURLInputProps {
 export const YouTubeURLInput: React.FC<YouTubeURLInputProps> = ({ onSubmit, isFetching, sx }) => {
   const [url, setUrl] = React.useState('');
   const [submitFlag, setSubmitFlag] = React.useState(false);
+  const [error, setError] = React.useState('');
 
   // Function to extract video ID from URL
-  function extractVideoID(videoURL: string): string | null {
+  const extractVideoID = (videoURL: string): string | null => {
     const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([^#&?]*).*/;
     const match = videoURL.match(regExp);
-    return (match && match[1]?.length == 11) ? match[1] : null;
+    return (match && match[1]?.length === 11) ? match[1] : null;
   }
 
   const videoID = extractVideoID(url);
@@ -30,12 +29,14 @@ export const YouTubeURLInput: React.FC<YouTubeURLInputProps> = ({ onSubmit, isFe
   const handleNewTranscript = (newTranscript: YTVideoTranscript) => {
     onSubmit(newTranscript.transcript); // Pass the transcript text to the onSubmit handler
     setSubmitFlag(false); // Reset submit flag after handling
+    setError(''); // Reset error message after successful fetch
   };
 
-  const { transcript, isFetching: isTranscriptFetching, isError, error } = useYouTubeTranscript(videoID && submitFlag ? videoID : null, handleNewTranscript);
+  const { transcript, isFetching: isTranscriptFetching, isError: isTranscriptError } = useYouTubeTranscript(videoID && submitFlag ? videoID : null, handleNewTranscript);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(event.target.value);
+    setError(''); // Reset error message when URL changes
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -43,21 +44,30 @@ export const YouTubeURLInput: React.FC<YouTubeURLInputProps> = ({ onSubmit, isFe
     setSubmitFlag(true); // Set flag to indicate a submit action
   };
 
+  React.useEffect(() => {
+    if (isTranscriptError) {
+      setError('Error fetching transcript. Please try again.');
+    }
+  }, [isTranscriptError]);
+
   return (
     <Box sx={{ mb: 1, ...sx }}>
       <form onSubmit={handleSubmit}>
-        <Input
-          required
-          type='url'
-          fullWidth
-          disabled={isFetching || isTranscriptFetching}
-          variant='outlined'
-          placeholder='Enter YouTube Video URL'
-          value={url}
-          onChange={handleChange}
-          startDecorator={<YouTubeIcon sx={{ color: '#f00' }} />}
-          sx={{ mb: 1.5, backgroundColor: 'background.popup' }}
-        />
+        <FormControl>
+          <InputLabel>Enter YouTube Video URL</InputLabel>
+          <Input
+            required
+            fullWidth
+            disabled={isFetching || isTranscriptFetching}
+            variant='outlined'
+            type='url'
+            value={url}
+            onChange={handleChange}
+            startDecorator={<YouTubeIcon sx={{ color: '#f00' }} />}
+            sx={{ mb: 1.5, backgroundColor: 'background.popup' }}
+          />
+          <FormHelperText error>{error}</FormHelperText>
+        </FormControl>
         <Button
           type='submit'
           variant='solid'
@@ -67,7 +77,6 @@ export const YouTubeURLInput: React.FC<YouTubeURLInputProps> = ({ onSubmit, isFe
         >
           Get Transcript
         </Button>
-        {isError && <div>Error fetching transcript. Please try again.</div>}
       </form>
     </Box>
   );
