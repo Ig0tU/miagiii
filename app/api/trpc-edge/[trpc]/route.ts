762b44042fc46a16f886df6f57a676a003f761ee
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:22 -0400

    improve

--- app/api/trpc-edge/[trpc]/route.ts
+++ app/api/trpc-edge/[trpc]/route.ts
@@ -1,11 +1,10 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-
-import { appRouterEdge } from '~/server/api/trpc.router-edge';
+import { AppRouterEdge } from '~/server/api/trpc.router-edge';
 import { createTRPCFetchContext } from '~/server/api/trpc.server';
 
 const handlerEdgeRoutes = (req: Request) =>
   fetchRequestHandler({
-    router: appRouterEdge,
+    router: AppRouterEdge,
     endpoint: '/api/trpc-edge',
     req,
     createContext: createTRPCFetchContext,
@@ -15,5 +14,9 @@ const handlerEdgeRoutes = (req: Request) =>
         : undefined,
   });
 
-export const runtime = 'edge';
-export { handlerEdgeRoutes as GET, handlerEdgeRoutes as POST };
+if (import.meta.url === import.meta.resolve('.')) {
+  export { handlerEdgeRoutes as GET, handlerEdgeRoutes as POST };
+  export const runtime = 'edge';
+} else {
+  export default handlerEdgeRoutes;
+}
