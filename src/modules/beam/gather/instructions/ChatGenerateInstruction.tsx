0f3f5e07a13ca14af151d37ad4641ac6c12d54a9
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:23 -0400

    improve

--- src/modules/beam/gather/instructions/ChatGenerateInstruction.tsx
+++ src/modules/beam/gather/instructions/ChatGenerateInstruction.tsx
@@ -1,120 +1,91 @@
-import * as React from 'react';
-
 import { Typography } from '@mui/joy';
-
-import { ChatMessage } from '../../../../apps/chat/components/message/ChatMessage';
-import { streamAssistantMessage } from '../../../../apps/chat/editors/chat-stream';
-
-import type { VChatMessageIn } from '~/modules/llms/llm.client';
-import { bareBonesPromptMixer } from '~/modules/persona/pmix/pmix';
-
+import { ChatMessage, streamAssistantMessage } from '../../../../apps/chat';
+import { VChatMessageIn } from '~/modules/llms/llm.client';
 import { DMessage } from '~/common/state/store-chats';
 import { getUXLabsHighPerformance } from '~/common/state/store-ux-labs';
-
-import type { BaseInstruction, ExecutionInputState } from './beam.gather.execution';
+import { BaseInstruction, ExecutionInputState } from './beam.gather.execution';
 import { GATHER_PLACEHOLDER } from '../../beam.config';
 import { beamCardMessageScrollingSx, beamCardMessageSx } from '../../BeamCard';
 import { getBeamCardScrolling } from '../../store-module-beam';
 
+type ChatGenerateMethods = 's-s0-h0-u0-aN-u';
 
-type ChatGenerateMethods =
-  | 's-s0-h0-u0-aN-u'; // sandwiches the existing history and the new proposals in between the System and User prompts of the Instruction
-
-export interface ChatGenerateInstruction extends BaseInstruction {
+interface ChatGenerateInstruction extends BaseInstruction {
   type: 'chat-generate';
-  display?:
-    | 'chat-message' /* default */
-    | 'character-count'
-    | 'mute';
+  display: 'chat-message' | 'character-count' | 'mute' | undefined;
   method: ChatGenerateMethods;
   systemPrompt: string;
   userPrompt: string;
-  // evalPrompt?: string;
 }
-
 
 /**
- * Merge Execution: uses a chain of Promises to queue up (cancellable) seuqential instructions.
+ * Merge Execution: uses a chain of Promises to queue up (cancellable) sequential instructions.
  */
-export async function executeChatGenerate(_i: ChatGenerateInstruction, inputs: ExecutionInputState, prevStepOutput: string): Promise<string> {
+export async function executeChatGenerate(
+  instruction: ChatGenerateInstruction,
+  inputs: ExecutionInputState,
+  prevStepOutput: string
+): Promise<string> {
+  if (instruction.method === 's-s0-h0-u0-aN-u') {
+    const history: VChatMessageIn[] = [
+      { role: 'system', content: _mixChatGeneratePrompt(instruction.systemPrompt, inputs.chatMessages.length, prevStepOutput) },
+      ...inputs.chatMessages.flatMap((m): VChatMessageIn[] => (m.role === 'user' || m.role === 'assistant') ? [{ role: m.role, content: m.text }] : []),
+      ...inputs.rayMessages.map((m): VChatMessageIn => ({ role: 'assistant', content: m.text })),
+      { role: 'user', content: _mixChatGeneratePrompt(instruction.userPrompt, inputs.rayMessages.length, prevStepOutput) },
+    ];
 
-  // build the input messages
-  if (_i.method !== 's-s0-h0-u0-aN-u')
-    throw new Error(`Unsupported Chat Generate method: ${_i.method}`);
-
-  const history: VChatMessageIn[] = [
-    // s
-    { role: 'system', content: _mixChatGeneratePrompt(_i.systemPrompt, inputs.rayMessages.length, prevStepOutput) },
-    // s0-h0-u0
-    ...inputs.chatMessages
-      .filter((m) => (m.role === 'user' || m.role === 'assistant'))
-      .map((m): VChatMessageIn => ({ role: (m.role !== 'assistant') ? 'user' : m.role, content: m.text })),
-    // aN
-    ...inputs.rayMessages
-      .map((m): VChatMessageIn => ({ role: 'assistant', content: m.text })),
-    // u
-    { role: 'user', content: _mixChatGeneratePrompt(_i.userPrompt, inputs.rayMessages.length, prevStepOutput) },
-  ];
-
-  // reset the intermediate message
-  Object.assign(inputs.intermediateDMessage, {
-    text: GATHER_PLACEHOLDER,
-    updated: undefined,
-  } satisfies Partial<DMessage>);
-
-  // update the UI
-  const onMessageUpdate = (update: Partial<DMessage>) => {
-    // in-place update of the intermediate message
-    Object.assign(inputs.intermediateDMessage, update);
-    if (update.text)
-      inputs.intermediateDMessage.updated = Date.now();
-
-    switch (_i.display) {
-      case 'mute':
-        return;
-
-      case 'character-count':
-        inputs.updateInstructionComponent(
-          <Typography level='body-xs' sx={{ opacity: 0.5 }}>{update.text?.length || 0} characters</Typography>,
-        );
-        return;
-
-      case 'chat-message':
-      default:
-        // recreate the UI for this
-        inputs.updateInstructionComponent(
-          <ChatMessage
-            message={inputs.intermediateDMessage}
-            fitScreen={true}
-            showAvatar={false}
-            adjustContentScaling={-1}
-            sx={!getBeamCardScrolling() ? beamCardMessageSx : beamCardMessageScrollingSx}
-          />,
-        );
-        return;
-    }
-  };
-
-  // LLM Streaming generation
-  return streamAssistantMessage(inputs.llmId, history, getUXLabsHighPerformance() ? 0 : 1, 'off', onMessageUpdate, inputs.chainAbortController.signal)
-    .then((status) => {
-      // re-throw errors, as streamAssistantMessage catches internally
-      if (status.outcome === 'aborted') {
-        // this message will be discarded as the abort status is checked in the next `catch`
-        throw new Error('Instruction Stopped.');
-      }
-      if (status.outcome === 'errored')
-        throw new Error(`Model execution error: ${status.errorMessage || 'Unknown error'}`);
-
-      // Proceed to the next step
-      return inputs.intermediateDMessage.text;
+    Object.assign(inputs.intermediateDMessage, {
+      text: GATHER_PLACEHOLDER,
+      updated: undefined,
     });
+
+    const onMessageUpdate = (update: Partial<DMessage>) => {
+      Object.assign(inputs.intermediateDMessage, update);
+      if (update.text) {
+        inputs.intermediateDMessage.updated = Date.now();
+      }
+
+      switch (instruction.display) {
+        case 'mute':
+          return;
+
+        case 'character-count':
+          inputs.updateInstructionComponent(
+            <Typography level='body-xs' sx={{ opacity: 0.5 }}>{update.text?.length || 0} characters</Typography>
+          );
+          return;
+
+        case 'chat-message':
+        default:
+          inputs.updateInstructionComponent(
+            <ChatMessage
+              message={inputs.intermediateDMessage}
+              fitScreen={true}
+              showAvatar={false}
+              adjustContentScaling={-1}
+              sx={!getBeamCardScrolling() ? beamCardMessageSx : beamCardMessageScrollingSx}
+            />
+          );
+          return;
+      }
+    };
+
+    return streamAssistantMessage(inputs.llmId, history, getUXLabsHighPerformance() ? 0 : 1, 'off', onMessageUpdate, inputs.chainAbortController.signal)
+      .then((status) => {
+        if (status.outcome === 'aborted') {
+          throw new Error('Instruction Stopped.');
+        }
+        if (status.outcome === 'errored') {
+          throw new Error(`Model execution error: ${status.errorMessage || 'Unknown error'}`);
+        }
+
+        return inputs.intermediateDMessage.text;
+      });
+  } else {
+    throw new Error(`Unsupported Chat Generate method: ${instruction.method}`);
+  }
 }
 
-
-function _mixChatGeneratePrompt(prompt: string, raysReady: number, prevStepOutput: string): string {
-  return bareBonesPromptMixer(prompt, undefined, {
-    '{{N}}': raysReady.toString(),
-    '{{PrevStepOutput}}': prevStepOutput,
-  });
+function _mixChatGeneratePrompt(prompt: string, chatCount: number, prevStepOutput: string): string {
+  return prompt.replace('{{N}}', chatCount.toString()).replace('{{PrevStepOutput}}', prevStepOutput);
 }
