1e0a23f3df1fa738fa28fda43308ebb3947ac99f
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:52 -0400

    improve

--- src/common/components/GitHubProjectIssueCard.tsx
+++ src/common/components/GitHubProjectIssueCard.tsx
@@ -1,35 +1,52 @@
 import * as React from 'react';
-
-import type { SxProps } from '@mui/joy/styles/types';
-import { Box, Card, Link as MuiLink, Typography } from '@mui/joy';
-import GitHubIcon from '@mui/icons-material/GitHub';
-
+import { GitHubIcon } from '@mui/icons-material';
+import {
+  Box,
+  Card,
+  Link as MuiLink,
+  SxProps,
+  Typography,
+} from '@mui/joy';
+import { string } from 'prop-types';
 
 export const GitHubProjectIssueCard = (props: {
-  issue: number,
-  text: string,
-  note?: string | React.ReactNode,
-  note2?: string | React.ReactNode,
-  sx?: SxProps
-}) =>
-  <Card variant='outlined' color='primary' sx={props.sx}>
+  issue: number;
+  text: string;
+  note?: string | React.ReactNode;
+  note2?: string | React.ReactNode;
+  sx?: SxProps;
+}) => (
+  <Card variant="outlined" color="primary" sx={props.sx}>
     <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
       <GitHubIcon />
-      <Typography level='body-sm'>
-        <MuiLink overlay href={`https://github.com/enricoros/big-AGI/issues/${props.issue}`} target='_blank'>
-          big-AGI #{props.issue}
+      <Typography level="body-sm">
+        <MuiLink
+          overlay
+          href={`https://github.com/enricoros/big-AGI/issues/${props.issue}`}
+          target="_blank"
+          fontSize="1.25rem"
+          sx={{ textDecoration: 'none' }}
+        >
+          <Typography color="neutral">big-AGI #{props.issue}</Typography>
         </MuiLink>
-        {' · '}{props.text}.
+        {' · '}
+        <Typography color="neutral">{props.text}.</Typography>
       </Typography>
     </Box>
     {!!props.note && (
-      <Typography level='body-sm' sx={{ mt: 1 }}>
+      <Typography level="body-sm" sx={{ mt: 1 }}>
         {props.note}
       </Typography>
     )}
     {!!props.note2 && (
-      <Typography level='body-sm' sx={{ mt: 1 }}>
+      <Typography level="body-sm" sx={{ mt: 1 }}>
         {props.note2}
       </Typography>
     )}
-  </Card>;
+  </Card>
+);
+
+GitHubProjectIssueCard.propTypes = {
+  issue: string.isRequired,
+  text: string.isRequired,
+};
