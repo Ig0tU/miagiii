f310398099ef9412b9b99b51fcc92dc9ed9fc2b8
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:15 -0400

    improve

--- src/apps/call/components/CallMessage.tsx
+++ src/apps/call/components/CallMessage.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 
-import { Chip, ColorPaletteProp, VariantProp } from '@mui/joy';
-import { SxProps } from '@mui/joy/styles/types';
-
+import { Chip, ColorPaletteProp, VariantProp, SxProps } from '@mui/joy';
 import type { VChatMessageIn } from '~/modules/llms/llm.client';
 
-
-export function CallMessage(props: {
-  text?: string | React.JSX.Element,
-  variant?: VariantProp, color?: ColorPaletteProp,
-  role: VChatMessageIn['role'],
-  sx?: SxProps,
-}) {
+export function CallMessage(
+  props: React.PropsWithChildren<{
+    text?: string | React.ReactNode;
+    variant?: VariantProp;
+    color?: ColorPaletteProp;
+    role: VChatMessageIn['role'];
+    sx?: SxProps<typeof Chip>;
+    key?: React.Key;
+  }> & React.RefAttributes<HTMLDivElement>
+) {
   const isUserMessage = props.role === 'user';
   return (
     <Chip
-      color={props.color} variant={props.variant}
+      ref={props.ref}
+      color={props.color}
+      variant={props.variant}
       sx={{
         alignSelf: isUserMessage ? 'end' : 'start',
         whiteSpace: 'break-spaces',
@@ -28,12 +31,11 @@ export function CallMessage(props: {
         // boxShadow: 'md',
         py: 1,
         px: 1.5,
-        ...(props.sx || {}),
+        ...(props.sx || {} as SxProps<typeof Chip>),
       }}
+      key={props.key}
     >
-
       {props.text}
-
     </Chip>
   );
 }
