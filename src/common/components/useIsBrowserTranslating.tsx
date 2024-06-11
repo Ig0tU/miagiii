be1c9bad265c4c32750c5293666c8b55f685fb23
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:07 -0400

    improve

--- src/common/components/useIsBrowserTranslating.tsx
+++ src/common/components/useIsBrowserTranslating.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
-
 import { Alert, IconButton } from '@mui/joy';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import { useUICounter } from '~/common/state/store-ui';
 
+type UseIsBrowserTranslatingReturnType = [boolean, () => void];
 
-export function useIsBrowserTranslating(timeout: number = 5000): boolean {
+export function useIsBrowserTranslating(timeout: number = 5000): UseIsBrowserTranslatingReturnType {
   // state
   const [isTranslating, setIsTranslating] = React.useState(false);
 
   React.useEffect(() => {
-
     const htmlElementMutationCallback: MutationCallback = (mutationsList, observer) => {
       for (const mutation of mutationsList) {
         // only look for class attribute changes
@@ -34,35 +33,45 @@ export function useIsBrowserTranslating(timeout: number = 5000): boolean {
     return () => observer.disconnect();
   }, []);
 
-  return isTranslating;
+  return [isTranslating, () => setIsTranslating(false)];
 }
 
-export function useBrowserTranslationWarning() {
+type UseBrowserTranslationWarningReturnType = React.ReactElement | null;
 
+export function useBrowserTranslationWarning(): UseBrowserTranslationWarningReturnType {
   // state
   const [hideWarning, setHideWarning] = React.useState(false);
 
   // external state
-  const isTranslating = useIsBrowserTranslating();
-  const { novel: lessThanFive, touch } = useUICounter('acknowledge-translation-warning', 5);
+  const [isTranslating, setIsTranslating] = useIsBrowserTranslating();
+  const { novel: lessThanFive, touch: touchUICounter } = useUICounter<{ novel: number, touch: number }>('acknowledge-translation-warning', { novel: 5, touch: 0 });
 
   const showWarning = isTranslating && !hideWarning && lessThanFive;
 
+  const touch = React.useCallback(() => {
+    if (typeof window !== 'undefined' && 'ontouchstart' in window) {
+      touchUICounter.touch();
+    } else {
+      touchUICounter.novel();
+    }
+  }, [touchUICounter]);
+
   return React.useMemo(() => showWarning ? (
     <Alert
+      key="translation-warning"
       variant='outlined' color='warning'
       startDecorator={<WarningRoundedIcon />}
       endDecorator={
-        <IconButton color='warning'>
-          <CloseRoundedIcon onClick={() => {
+        <IconButton color='warning' sx={{ ml: 1 }}>
+          <CloseRoundedIcon title="Dismiss" onClick={() => {
             setHideWarning(true);
             touch();
           }} />
         </IconButton>
       }
+      onTransitionClose={() => setIsTranslating(false)}
     >
-      This page is being translated by your browser. It is recommended to turn OFF translation as it may cause issues,
-      such as &quot;a client-side exception has occurred&quot;.
+      <div dangerouslySetInnerHTML={{ __html: 'This page is being translated by your browser. It is recommended to turn OFF translation as it may cause issues, such as &quot;<span style="color: red;">a client-side exception has occurred</span>&quot;.' }} />
     </Alert>
   ) : null, [showWarning, touch]);
 }
