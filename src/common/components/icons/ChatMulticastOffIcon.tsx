27326dd08a8f219917cdf8e9c291b6ddb3113193
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:04 -0400

    improve

--- src/common/components/icons/ChatMulticastOffIcon.tsx
+++ src/common/components/icons/ChatMulticastOffIcon.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
-
-import { SvgIcon, SvgIconProps } from '@mui/joy';
+import { SvgIcon, SvgIconProps } from '@mui/material'; // Corrected import from '@mui/joy' to '@mui/material'
 
 /*
  * Source: the PodcastsIcon from '@mui/icons-material/Podcasts';
  */
-export function ChatMulticastOffIcon(props: SvgIconProps) {
+export const ChatMulticastOffIcon: React.FC<SvgIconProps> = (props) => {
   return (
     <SvgIcon
-      viewBox='0 0 24 24'
-      width={24}
-      height={24}
+      viewBox="0 0 24 24"
       {...props}
     >
-      <path d='M14 12c0 .74-.4 1.38-1 1.72V22h-2v-8.28c-.6-.35-1-.98-1-1.72 0-1.1.9-2 2-2s2 .9 2 2'></path>
+      <path d='M14 12c0 .74-.4 1.38-1 1.72V22h-2v-8.28c-.6-.35-1-.98-1-1.72 0-1.1.9-2 2-2s2 .9 2 2' />
     </SvgIcon>
   );
 }
