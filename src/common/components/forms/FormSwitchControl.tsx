f13cf5f014d9cf23817b5c5701e065fa9e18332e
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:52 -0400

    improve

--- src/common/components/forms/FormSwitchControl.tsx
+++ src/common/components/forms/FormSwitchControl.tsx
@@ -4,25 +4,40 @@ import { FormControl, Switch } from '@mui/joy';
 
 import { FormLabelStart } from './FormLabelStart';
 
-
 /**
  * Switch control
  */
 export function FormSwitchControl(props: {
-  title: string | React.JSX.Element, description?: string | React.JSX.Element,
-  on?: string, off?: string, fullWidth?: boolean,
-  checked: boolean, onChange: (on: boolean) => void,
+  title: string | React.ReactNode,
+  description?: string | React.ReactNode,
+  on?: string,
+  off?: string,
+  fullWidth?: boolean,
+  checked: boolean,
+  onChange: (on: boolean) => void,
   disabled?: boolean,
+  defaultChecked?: boolean,
+  name?: string,
+  required?: boolean,
 }) {
   return (
-    <FormControl orientation='horizontal' disabled={props.disabled} sx={{ flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center' }}>
-      <FormLabelStart title={props.title} description={props.description} />
+    <FormControl
+      orientation='horizontal'
+      disabled={props.disabled}
+      sx={{ flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center', margin: '0.5rem' }}
+      required={props.required}
+      name={props.name}
+    >
+      <FormLabelStart title={props.title} description={props.description} fontSize='1rem' />
       <Switch
         checked={props.checked}
+        defaultChecked={props.defaultChecked}
         onChange={event => props.onChange(event.target.checked)}
         endDecorator={props.checked ? props.on || 'On' : props.off || 'Off'}
         sx={props.fullWidth ? { flexGrow: 1 } : undefined}
         slotProps={{ endDecorator: { sx: { minWidth: 26 } } }}
+        color='primary'
+        size='medium'
       />
     </FormControl>
   );
