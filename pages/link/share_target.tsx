f3ab5a92bfa725e809092fbd9860389138c82e5d
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:37 -0400

    improve

--- pages/link/share_target.tsx
+++ pages/link/share_target.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
-
-import { Alert, Box, Button, Typography } from '@mui/joy';
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Stack,
+  Typography,
+} from '@mui/joy';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
-import { setComposerStartupText } from '../../src/apps/chat/components/composer/store-composer';
-
+import { setComposerStartupText, useComposerStartupText } from '../../src/apps/chat/components/composer/store-composer';
 import { callBrowseFetchPage } from '~/modules/browse/browse.client';
-
-import { LogoProgress } from '~/common/components/LogoProgress';
-import { asValidURL } from '~/common/util/urlUtils';
-import { navigateToIndex, useRouterQuery } from '~/common/app.routes';
+import { asValidURL, navigateToIndex, useRouterQuery } from '~/common/app.routes';
 import { withLayout } from '~/common/layout/withLayout';
-
 
 /**
  * This page will be invoked on mobile when sharing Text/URLs/Files from other APPs
@@ -25,110 +26,106 @@ import { withLayout } from '~/common/layout/withLayout';
 function AppShareTarget() {
   // state
   const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
-  const [intentText, setIntentText] = React.useState<string | null>(null);
-  const [intentURL, setIntentURL] = React.useState<string | null>(null);
   const [isDownloading, setIsDownloading] = React.useState(false);
+  const composerStartupText = useComposerStartupText();
 
   // external state
   const { url: queryUrl, text: queryText } = useRouterQuery<{
-    url: string | string[] | undefined,
-    text: string | string[] | undefined,
+    url: string | string[] | undefined;
+    text: string | string[] | undefined;
   }>();
-
-  const queueComposerTextAndLaunchApp = React.useCallback((text: string) => {
-    setComposerStartupText(text);
-    void navigateToIndex(true);
-  }, []);
-
 
   // Detect the share Intent from the query
   React.useEffect(() => {
     // skip when query is not parsed yet
     let queryTextItem = queryUrl || queryText || null;
-    if (!queryTextItem)
-      return;
+    if (!queryTextItem) return;
 
     // single item from the query
-    if (Array.isArray(queryTextItem))
-      queryTextItem = queryTextItem[0];
+    if (Array.isArray(queryTextItem)) queryTextItem = queryTextItem[0];
 
     // check if the item is a URL
     const url = asValidURL(queryTextItem);
-    if (url)
-      setIntentURL(url);
-    else if (queryTextItem)
-      setIntentText(queryTextItem);
-    else
-      setErrorMessage('No text or url. Received: ' + JSON.stringify({ queryText, queryUrl }));
-
+    if (url) setIntentURL(url);
+    else if (queryTextItem) setIntentText(queryTextItem);
+    else setErrorMessage('No text or url. Received: ' + JSON.stringify({ queryText, queryUrl }));
   }, [queryText, queryUrl]);
 
+  // Handle URL download and update composer
+  const setIntentURL = React.useCallback((url: string) => {
+    setIsDownloading(true);
+    callBrowseFetchPage(url)
+      .then((page) => {
+        if (page.stopReason !== 'error') {
+          let pageContent =
+            page.content.markdown || page.content.text || page.content.html || '';
+          if (pageContent)
+            pageContent = `\n\n\`\`\`${url}\n${pageContent}\n\`\`\`\n`;
+          setComposerStartupText(pageContent);
+          navigateToIndex(true);
+        } else setErrorMessage('Could not read any data' + (page.error ? `: ${page.error}` : ''));
+      })
+      .catch((error) => setErrorMessage(error?.message || error || 'Unknown error'))
+      .finally(() => setIsDownloading(false));
+  }, []);
 
-  // Text -> Composer
-  React.useEffect(() => {
-    if (intentText)
-      queueComposerTextAndLaunchApp(intentText);
-  }, [intentText, queueComposerTextAndLaunchApp]);
-
-
-  // URL -> download -> Composer
-  React.useEffect(() => {
-    if (intentURL) {
-      setIsDownloading(true);
-      callBrowseFetchPage(intentURL)
-        .then(page => {
-          if (page.stopReason !== 'error') {
-            let pageContent = page.content.markdown || page.content.text || page.content.html || '';
-            if (pageContent)
-              pageContent = '\n\n```' + intentURL + '\n' + pageContent + '\n```\n';
-            queueComposerTextAndLaunchApp(pageContent);
-          } else
-            setErrorMessage('Could not read any data' + page.error ? ': ' + page.error : '');
-        })
-        .catch(error => setErrorMessage(error?.message || error || 'Unknown error'))
-        .finally(() => setIsDownloading(false));
-    }
-  }, [intentURL, queueComposerTextAndLaunchApp]);
-
+  // Handle text and update composer
+  const setIntentText = React.useCallback((text: string) => {
+    setComposerStartupText(text);
+    navigateToIndex(true);
+  }, []);
 
   return (
-
-    <Box sx={{
-      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-      flexGrow: 1,
-    }}>
-
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexGrow: 1,
+      }}
+    >
       {/* Logo with Circular Progress  */}
-      <LogoProgress showProgress={isDownloading} />
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ mb: 2 }}
+      >
+        <CircularProgress size="sm" value={isDownloading ? 100 : 0} />
+      </Stack>
 
       {/* Title */}
-      <Typography level='title-lg' sx={{ mt: 2, mb: 1 }}>
-        {isDownloading ? 'Loading...' : errorMessage ? '' : intentURL ? 'Done' : 'Receiving...'}
+      <Typography
+        level="title-lg"
+        sx={{ mt: 2, mb: 1, textAlign: 'center' }}
+      >
+        {isDownloading ? 'Loading...' : errorMessage ? '' : composerStartupText ? 'Done' : 'Receiving...'}
       </Typography>
 
       {/* Possible Error */}
-      {errorMessage && <>
-        <Alert variant='soft' color='danger' sx={{ my: 1 }}>
+      {errorMessage && (
+        <Alert variant="soft" color="danger" sx={{ my: 1 }}>
           <Typography>{errorMessage}</Typography>
         </Alert>
-        <Button
-          variant='solid' color='danger'
-          onClick={() => navigateToIndex()}
-          endDecorator={<ArrowBackIcon />}
-          sx={{ mt: 2 }}
-        >
-          Cancel
-        </Button>
-      </>}
+      )}
+
+      {/* Back Button */}
+      <Button
+        variant="solid" color="neutral"
+        onClick={() => navigateToIndex()}
+        endDecorator={<ArrowBackIcon />}
+        sx={{ mt: 2 }}
+      >
+        Back
+      </Button>
 
       {/* URL under analysis */}
-      <Typography level='body-xs'>
-        {intentURL}
+      <Typography level="body-xs" sx={{ mt: 2 }}>
+        {composerStartupText || queryUrl || queryText}
       </Typography>
-    </Box>
-
+    </Container>
   );
-
 }
 
 /**
