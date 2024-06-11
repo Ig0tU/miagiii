3d44d5c7c09c7c90e47679e10fc331a17084ed2f
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:19:02 -0400

    improve

--- src/common/components/KeyStroke.tsx
+++ src/common/components/KeyStroke.tsx
@@ -6,33 +6,34 @@ import { SxProps } from '@mui/joy/styles/types';
 import { hideOnMobile } from '~/common/app.theme';
 import { isMacUser } from '~/common/util/pwaUtils';
 
-
-export function platformAwareKeystrokes(text: string) {
-  return isMacUser
-    ? text
+/**
+ * Converts a keystroke string to a platform-aware representation.
+ * @param text The keystroke string to convert.
+ * @returns The converted keystroke string.
+ */
+export function platformAwareKeystrokes(text: string): string {
+  if (isMacUser) {
+    return text
       .replaceAll('Ctrl', '⌘' /* Command */)
       .replaceAll('Alt', '⌥' /* Option */)
-      .replaceAll('Shift', '⇧')
-    // Optional: Replace "Enter" with "Return" if you want to align with Mac keyboard labeling
-    // .replaceAll('Enter', 'Return')
-    : text;
+      .replaceAll('Shift', '⇧');
+  }
+  return text;
 }
 
 /**
  * Shows a shortcut combo in a nicely presented dark box.
+ * @param props The props to use for the `KeyStroke` component.
+ * @returns The rendered `KeyStroke` component.
  */
-export function KeyStroke(props: { combo: string, dark?: boolean, sx?: SxProps }) {
+export function KeyStroke(props: { combo: string | null | undefined, dark?: boolean, sx?: SxProps }) {
   return (
-    <Chip variant={props.dark ? 'solid' : 'outlined'} color='neutral' sx={{ ...hideOnMobile, ...props.sx }}>
-      {platformAwareKeystrokes(props.combo)}
+    <Chip
+      variant={props.dark ? 'solid' : 'outlined'}
+      color='neutral'
+      sx={{ ...hideOnMobile, ...(props.sx || {}) }}
+    >
+      {props.combo && platformAwareKeystrokes(props.combo)}
     </Chip>
-    // <Box sx={{
-    //   position: 'relative', display: 'inline-block', px: 1, py: 0.5,
-    //   bg: 'rgba(0,0,0,0.75)', color: 'white', borderRadius: 1,
-    //   fontSize: 12, fontWeight: 'md', lineHeight: 1, whiteSpace: 'nowrap',
-    // }}>
-    //   <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', bg: 'rgba(0,0,0,0.5)', borderRadius: 1 }} />
-    //   {props.combo}
-    // </Box>
   );
 }
