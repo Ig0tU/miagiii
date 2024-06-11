ed713c6638ef05f1c62ebc289c668f295d562d92
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:13:32 -0400

    improve

--- src/apps/call/components/CallAvatar.tsx
+++ src/apps/call/components/CallAvatar.tsx
@@ -1,36 +1,55 @@
 import * as React from 'react';
-
 import { Avatar, Box } from '@mui/joy';
-
 import { animationScalePulse } from '~/common/util/animUtils';
 
+type CallAvatarProps = {
+  symbol: string;
+  imageUrl?: string;
+  isRinging?: boolean;
+  onClick: () => void;
+};
 
-export function CallAvatar(props: { symbol: string, imageUrl?: string, isRinging?: boolean, onClick: () => void }) {
+export function CallAvatar(props: CallAvatarProps) {
   return (
     <Avatar
       onClick={props.onClick}
       src={props.imageUrl}
+      alt={props.imageUrl || props.symbol}
+      role="button"
+      aria-label={`${props.isRinging ? 'Ringing ' : ''}Call with ${props.symbol}`}
+      title={props.symbol}
+      tabIndex={0}
+      cursor="pointer"
       sx={{
+        width: { xs: '10rem', md: '11.5rem' },
+        height: { xs: '10rem', md: '11.5rem' },
+        borderRadius: '50%',
+        transition: 'transform 0.3s ease',
+        userSelect: 'none',
+        overflow: 'hidden',
+        outline: 'none',
         '--Avatar-size': { xs: '10rem', md: '11.5rem' },
         backgroundColor: 'background.popup',
         boxShadow: !props.imageUrl ? 'sm' : null,
         fontSize: { xs: '6rem', md: '7rem' },
+        ...(props.isRinging
+          ? { animation: `${animationScalePulse} 1.4s ease-in-out infinite` }
+          : {}),
       }}
     >
-
-      {/* As fallback, show the large Persona Symbol */}
       {!props.imageUrl && (
         <Box
           sx={{
-            ...(props.isRinging
-              ? { animation: `${animationScalePulse} 1.4s ease-in-out infinite` }
-              : {}),
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
           {props.symbol}
         </Box>
       )}
-
     </Avatar>
   );
 }
