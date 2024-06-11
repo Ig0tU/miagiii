3c8e478b4e0200430f6ea36ee6c336e2ceff2fba
Ig0tU <124455599+Ig0tU@users.noreply.github.com>
2024-06-11 12:18:15 -0400

    improve

--- src/common/layout/optima/components/PageDrawerHeader.tsx
+++ src/common/layout/optima/components/PageDrawerHeader.tsx
@@ -1,45 +1,49 @@
 import * as React from 'react';
-
-import type { SxProps } from '@mui/joy/styles/types';
+import { SxProps } from '@mui/joy/styles';
 import { Box, IconButton, Typography } from '@mui/joy';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
+export type PageDrawerHeaderProps = {
+  title: string;
+  onClose: () => void;
+  sx?: SxProps;
+  children?: React.ReactNode;
+};
 
-export const PageDrawerHeader = (props: {
-  title: string,
-  onClose: () => void,
-  sx?: SxProps,
-  children?: React.ReactNode,
-}) =>
+export const PageDrawerHeader = ({
+  title,
+  onClose,
+  sx = {},
+  children,
+}: PageDrawerHeaderProps) => (
   <Box
-    // variant='soft'
-    // invertedColors
     sx={{
       minHeight: 'var(--AGI-Nav-width)',
       px: 1,
-
-      // style
       backgroundColor: 'background.popup',
-      // borderLeft: 'none',
-      // borderRight: 'none',
-      // borderTop: 'none',
-      // borderTopRightRadius: 'var(--AGI-Optima-Radius)',
-
-      // layout
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
+      ...sx,
     }}
   >
+    {children || <IconButton disabled />}
 
-    {props.children || <IconButton disabled />}
-
-    <Typography level='title-md'>
-      {props.title}
+    <Typography
+      level="title-md"
+      fontWeight="md"
+      color="neutral.body"
+    >
+      {title}
     </Typography>
 
-    <IconButton aria-label='Close Drawer' size='sm' onClick={props.onClose}>
-      <CloseRoundedIcon />
+    <IconButton
+      aria-label="Close Drawer"
+      size="sm"
+      onClick={onClose}
+      disabled={!onClose}
+    >
+      <CloseRoundedIcon fontSize="small" alt="Close Drawer" />
     </IconButton>
-
-  </Box>;
+  </Box>
+);
